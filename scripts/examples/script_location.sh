31795d379b8a94e37465677f5776c47a476a0f64
Fred T. Hamster <fred@mycroft.gruntose.blurgh>
2012-01-13 22:57:50 -0500

    example of auto-location of a running scripts path in the file system. does not work for a file that is 'source'd into another bash script.

---
+++ scripts/examples/script_location.sh
@@ -0,0 +1,7 @@
+
+# find out the location where this script is running from.  this will not
+# work properly in a bash script that is included via 'source' or '.'.
+# the first letter of each command is escaped to eliminate the danger of
+# personal aliases or functions disrupting the results.
+ORIGINATING_FOLDER="$( \cd "$(\dirname "$0")" && \pwd )"
+
