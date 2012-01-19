ef9fa75369c57548fe41e5ce3724b7f7c40b983c
Chris Koeritz <fred@gruntose.com>
2012-01-19 09:08:45 -0800

    finding a safe place for the custom examples.

---
+++ scripts/custom/examples/fred/c_variables.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# this set of files is our personal overrides.
+
+# The quartz directory has personalized items.
+export QUARTZDIR=$HOME/quartz
+
+# The gruntose web site is expected to reside below, if it exists at all.
+export WEB_DIR=$HOME/web
+if [ "$(hostname)" = "zooty.koeritz.com" ]; then
+  export WEB_DIR=/var/www
+fi
+
+# point to our local certificate for ssh usage.
+export SVN_SSH="ssh -i $HOME/.ssh/id_dsa_sourceforge"
+
+# Error and success noises for CLAM.
+export CLAM_ERROR_SOUND='/z/walrus/media/sounds/effects/bwaaang.wav /z/walrus/media/sounds/cartoons/doh4.wav'
+export CLAM_FINISH_SOUND='/z/walrus/media/sounds/cartoons/meepmeep.wav'
+
+# Setup for nethack adventure.
+export NETHACKOPTIONS='name:Manjusri-W,dogname:Fred,catname:Zonker'
+
+# mail setup for home machines.
+export REPLYTO=fred@gruntose.com
+export from="Fred T. Hamster <fred@gruntose.com>"
+
+# set our browser for seti and others that use the variable.
+export BROWSER=/usr/bin/firefox
+
+# editor and other mixed settings...
+export VISUAL=$(which vim)
+#export JAVA_HOME=/usr/java/jdk1.6.0_11
+#export ANT_HOME=/usr/local/eclipse/plugins/org.apache.ant_1.7.0.v200803061910/
+# special settings for win32 and svn.
+if [ "$OS" == "Windows_NT" ]; then
+  export EDITOR=$(which gvim)
+  export JAVA_HOME="c:/Program Files/java/jdk1.6.0_11"
+fi
+
+# this hideous mess is necessitated by our not having found the source of the
+# settings yet.  we override a few colors that look bad on a dark background.
+export LS_COLORS='no=00:fi=00:di=01;37:ln=00;36:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=41;33;01:ex=00;32:*.cmd=00;32:*.exe=01;32:*.com=01;32:*.bat=01;32:*.btm=01;32:*.dll=01;32:*.tar=00;31:*.tbz=00;31:*.tgz=00;35:*.rpm=00;33:*.deb=00;33:*.arj=00;31:*.taz=00;31:*.lzh=00;31:*.zip=00;35:*.zoo=00;31:*.z=00;31:*.Z=00;31:*.gz=00;35:*.bz2=00;31:*.tb2=00;31:*.tz2=00;31:*.tbz2=00;31:*.avi=01;35:*.bmp=01;35:*.fli=01;35:*.gif=01;35:*.jpg=01;35:*.jpeg=01;35:*.mng=01;35:*.mov=01;35:*.mpg=01;35:*.pcx=01;35:*.pbm=01;35:*.pgm=01;35:*.png=01;35:*.ppm=01;35:*.tga=01;35:*.tif=01;35:*.xbm=01;35:*.xpm=01;35:*.dl=01;35:*.gl=01;35:*.wmv=01;35:*.aiff=00;32:*.au=00;32:*.mid=00;32:*.mp3=00;32:*.ogg=00;32:*.voc=00;32:*.wav=00;32:'
+
+# we set this to avoid paying for slow app signing on win32.
+export NO_SIGNING=true
+
+# language settings for our common need.
+#export LANGUAGE=en_US
+#export LC_ALL=en_US
+#export LANG=en_US
+
---
+++ scripts/custom/examples/readme.txt
@@ -0,0 +1,7 @@
+
+
+this folder has some examples of how various people (or one person right now)
+do their custom scripts.
+
+the scripts should have the same name as the main yeti script's corresponding
+name, but with a "c_" in front.
