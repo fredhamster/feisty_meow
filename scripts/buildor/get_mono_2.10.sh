508e116f343fe8c3e425cdcd9bb7d85ef8b607f3
Fred T. Hamster <fred@serene.koeritz.org>
2012-02-01 19:29:39 -0500

    added script to grab and install mono 2.10, from nathanb's web site.

---
+++ scripts/buildor/get_mono_2.10.sh
@@ -0,0 +1,7 @@
+
+mkdir mono-2.10
+cd mono-2.10
+wget --no-check-certificate https://github.com/nathanb/iws-snippets/raw/master/mono-install-scripts/ubuntu/install_mono-2.10.sh
+chmod 755 install_mono-2.10.sh
+./install_mono-2.10.sh
+
