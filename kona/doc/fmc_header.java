408bb212869668cbf287c1e177ea2f9a8d0e0491
Chris Koeritz <fred@gruntose.com>
2012-02-15 14:48:03 -0500

    added/updated some headers for usage in feisty meow code.  the uva_header covers assets written for uva, whereas fmc_header is for assets where feisty meow concerns owns the copyright.

---
+++ kona/doc/fmc_header.java
@@ -0,0 +1,12 @@
+
+//////////////
+// Name   : {class name}
+// Author : {your name}
+// Rights : Copyright (c) 2012-$now By Feisty Meow Concerns, Ltd.
+//////////////
+// This script is free software; you can modify/redistribute it under the terms
+// of the GNU General Public License. [ http://www.gnu.org/licenses/gpl.html ]
+// Feel free to send updates to: [ fred@feistymeow.org ]
+//////////////
+
+
---
+++ kona/doc/uva_header.java
@@ -0,0 +1,12 @@
+
+//////////////
+// Name   : {class name}
+// Author : {your name}
+// Rights : Copyright (c) 2012-$now By University of Virginia
+//////////////
+// This file is free software; you can modify/redistribute it under the terms
+// of the Apache License v2.0: http://www.apache.org/licenses/LICENSE-2.0
+// Feel free to send updates to: [ koeritz@virginia.edu ]
+//////////////
+
+
