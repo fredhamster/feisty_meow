71e979d693fec6edf66a1201572659e37c359007
Chris Koeritz <fred@gruntose.com>
2012-02-21 20:18:01 -0500

    hmmm, does not seem to be finding these win32_help files yet.

--- scripts/generator/vis_stu_vars.sh
+++ scripts/generator/vis_stu_vars.sh
@@ -12,9 +12,7 @@ function print_usage {
   echo ""
 }
 
-if [ "$OPERATING_SYSTEM" == "WIN32" ]; then
-  export PATH=$PRODUCTION_DIR/win32_help:$PATH
-fi
+export PATH=$PRODUCTION_DIR/win32_help:$PATH
 
 # we try to use the most recent compiler location, and work backwards as
 # needed for the supported range (10 = vs 2010, 9 = vs 2008, 8 = vs 2005).
