1a73073afe1aa28972a45a3def4c0900dfa07507
Chris Koeritz <fred@gruntose.com>
2012-02-21 23:30:56 -0500

    fix for missing semicolon.

--- scripts/files/zapdirs.pl
+++ scripts/files/zapdirs.pl
@@ -21,7 +21,7 @@
 
 require "zap_the_dir.pl";
 
-$DEV_NULL = "&>/dev/null"
+$DEV_NULL = "&>/dev/null";
 #hmmm: move this to a useful location in a perl library.
 if ($OS eq "UNIX") {
   $FIND_ENDING = "';'";
