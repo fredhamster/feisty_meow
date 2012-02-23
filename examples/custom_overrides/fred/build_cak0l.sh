475bec2b699411d95fae1f64f124a0fa2d3c45e2
Chris Koeritz <fred@gruntose.com>
2012-02-23 11:49:56 -0500

    whoops, had to wrap these.

--- examples/custom_overrides/fred/build_cak0l.sh
+++ examples/custom_overrides/fred/build_cak0l.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
-pushd ~/xsede/code/cak0l/trunk
-ant -Dbuild.targetArch=64 build
-popd
+
+function rebuild_cak0l()
+{
+  pushd ~/xsede/code/cak0l/trunk
+  ant clean
+  ant -Dbuild.targetArch=64 build
+  popd
+}
+
+function build_cak0l()
+{
+  pushd ~/xsede/code/cak0l/trunk
+  ant -Dbuild.targetArch=64 build
+  popd
+}
+
 
--- examples/custom_overrides/fred/rebuild_cak0l.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-pushd ~/xsede/code/cak0l/trunk
-ant clean
-ant -Dbuild.targetArch=64 build
-popd
-
