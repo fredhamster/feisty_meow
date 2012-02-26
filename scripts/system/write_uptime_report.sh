9822bd6f2cdfe1fa0b5e7e7721f9a2bcccc66f4f
root <root@serene.koeritz.org>
2012-02-26 15:29:17 -0500

    stopped needing mdate to be built by using date stringer instead.

--- scripts/system/write_uptime_report.sh
+++ scripts/system/write_uptime_report.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # writes an uptime report to a file in the home directory which is named
 # after the current machine's hostname.
+source "$FEISTY_MEOW_DIR/scripts/core/functions.sh"
 export hosty=$(hostname | sed -e 's/^\([^.]*\).*$/\1/')
 export REPORT_FILE="$HOME/${hosty}_uptime.log"
-echo "$($BINDIR/mdate) -- $(uptime)" >>"$REPORT_FILE" 2>&1 
+echo "$(date_stringer) -- $(uptime)" >>"$REPORT_FILE" 2>&1 
