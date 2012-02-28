5ff2c19a273624390ba6e0fe61bc47e049274e4a
Chris Koeritz <fred@gruntose.com>
2012-02-28 11:15:51 -0500

    updated headers to be more javadocy

--- doc/headers/fmc_header.java
+++ doc/headers/fmc_header.java
@@ -1,12 +1,10 @@
 
-//////////////
-// Name   : {class name}
-// Author : Chris Koeritz
-// Rights : Copyright (c) 2012-$now By Feisty Meow Concerns, Ltd.
-//////////////
-// This file is free software; you can modify/redistribute it under the terms
-// of the GNU General Public License. [ http://www.gnu.org/licenses/gpl.html ]
-// Feel free to send updates to: [ fred@feistymeow.org ]
-//////////////
-
+/**
+ * {summary of class}
+ * @author Chris Koeritz
+ * @copyright Copyright (c) 2012-$now By Feisty Meow Concerns, Ltd.
+ * @license This file is free software; you can modify/redistribute it under the terms of the GNU
+ *          General Public License. [ http://www.gnu.org/licenses/gpl.html ]
+ * @contribute Feel free to send updates to: [ fred@feistymeow.org ]
+ */
 
--- doc/headers/uva_header.java
+++ doc/headers/uva_header.java
@@ -1,12 +1,9 @@
 
-//////////////
-// Name   : {class name}
-// Author : Chris Koeritz
-// Rights : Copyright (c) 2012-$now By University of Virginia
-//////////////
-// This file is free software; you can modify/redistribute it under the terms
-// of the Apache License v2.0: http://www.apache.org/licenses/LICENSE-2.0
-// Feel free to send updates to: [ koeritz@virginia.edu ]
-//////////////
-
+/**
+ * {summary of class}
+ * @author Chris Koeritz
+ * @copyright Copyright (c) 2012-$now By University of Virginia
+ * @license This file is free software; you can modify and redistribute it under the terms of the
+ *          Apache License v2.0: http://www.apache.org/licenses/LICENSE-2.0
+ */
 
