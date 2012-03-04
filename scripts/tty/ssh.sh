3a884022a9aa69a5b682b525bcadf308f14cd8ae
Chris Koeritz <fred@gruntose.com>
2012-03-03 23:23:31 -0500

    trapping git calling us.

--- scripts/tty/ssh.sh
+++ scripts/tty/ssh.sh
@@ -19,9 +19,12 @@ echo hey we be on wik dese parms $* >~/crap.txt
 \ssh -i "$keyfile" -X $*
 
 if [ $? -eq 0 ]; then
-  # re-run the terminal labeller after coming back from ssh.
-  # we check the exit value because we don't want to update this for a failed connection.
-  bash $FEISTY_MEOW_SCRIPTS/tty/label_terminal_with_infos.sh
+  # we don't want to emit anything extra if this is being driven by git.
+  if [ -z "$(echo $* | grep git)" ]; then
+    # re-run the terminal labeller after coming back from ssh.
+    # we check the exit value because we don't want to update this for a failed connection.
+    bash $FEISTY_MEOW_SCRIPTS/tty/label_terminal_with_infos.sh
+  fi
 fi
 
 
