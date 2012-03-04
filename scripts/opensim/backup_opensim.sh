d45b869795db30f55c3304211908172742d5cf07
Chris Koeritz <fred@gruntose.com>
2012-03-03 21:58:38 -0500

    precaution that seemed to fix weirdness in backup osgrid script.

--- scripts/opensim/backup_opensim.sh
+++ scripts/opensim/backup_opensim.sh
@@ -5,11 +5,10 @@ source "$FEISTY_MEOW_SCRIPTS/core/functions.sh"
 
 host=$(echo $(hostname) | sed -e 's/\([^.]*\)\..*/\1/')
 
-bkupname=opensim_bkup_${host}_$(date_stringer).mysql_bkup
-mysqldump -u griduser -p$(cat $HOME/.secrets/opensim_db_password.txt) opensim > $bkupname
+bkupname="opensim_bkup_${host}_$(date_stringer).mysql_bkup"
+mysqldump -u griduser -p$(cat $HOME/.secrets/opensim_db_password.txt) opensim > "$bkupname"
 # note that the above assumes the database is called "opensim".  it might be
 # called opengrid instead, based on the setup procedure that was followed.
 # likewise the user might be someone other than "griduser".
-gzip $bkupname
-
+gzip "$bkupname"
 
