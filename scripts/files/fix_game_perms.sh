5e087d2d7339002bd77810b198c55be8e57a71e7
Chris Koeritz <fred@gruntose.com>
2012-03-05 23:22:16 -0500

    new files for newer examples, now using /home/games for transient installed games rather than /usr/local/games.  fits better with our current style of having a huge home partition and smaller other partitions.

--- scripts/files/fix_game_perms.sh
+++ scripts/files/fix_game_perms.sh
@@ -3,6 +3,6 @@
 # fixes the permissions of files in the local games directory so that
 # everyone can run or read them if the owner could.
 
-find /usr/local/games -type f -perm /100 -exec chmod a+x {} ';'
-find /usr/local/games -type f -perm /400 -exec chmod a+r {} ';'
+find /home/games -type f -perm /100 -exec chmod a+x {} ';'
+find /home/games -type f -perm /400 -exec chmod a+r {} ';'
 
