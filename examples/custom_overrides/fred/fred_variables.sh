01201c8cc83d6f13a27461a98d3dbbe0c5e531ac
Chris Koeritz <fred@gruntose.com>
2012-03-11 13:23:55 -0400

    updated to not redefine my vars.

--- examples/custom_overrides/fred/fred_variables.sh
+++ examples/custom_overrides/fred/fred_variables.sh
@@ -1,52 +1,57 @@
 
 # these are my personal overrides.  --fred.
 
-# The quartz directory has *really* personalized items.
-export QUARTZDIR=$HOME/quartz
+if [ ! -z "$NETHACKOPTIONS" ]; then
+  # if we don't see the nethack variable defined, this probably hasn't run yet.
 
-# The gruntose web site is expected to reside below, if it exists at all.
-export WEB_DIR=$HOME/web
-if [ "$(hostname)" = "zooty.koeritz.com" ]; then
-  export WEB_DIR=/var/www
+  # The quartz directory has *really* personalized items.
+  export QUARTZDIR=$HOME/quartz
+
+  # The gruntose web site is expected to reside below, if it exists at all.
+  export WEB_DIR=$HOME/web
+  if [ "$(hostname)" = "zooty.koeritz.com" ]; then
+    export WEB_DIR=/var/www
+  fi
+
+  # add a bunch of folders to the list for checkin & checkout.  these are
+  # definitely personal, and some of them are fairly dated (the yeti and
+  # hoople folders, for example).
+  REPOSITORY_LIST+=" cloud gamesave hoople hoople1 hoople2 quartz web yeti xsede/xsede_tests xsede/code/cak0l/trunk"
+
+  # point to our local certificate for ssh usage.
+  export SVN_SSH="ssh -i $HOME/.ssh/id_dsa_sourceforge"
+
+  # Error and success noises for CLAM.
+  export CLAM_ERROR_SOUND='/z/walrus/media/sounds/effects/bwaaang.wav /z/walrus/media/sounds/cartoons/doh4.wav'
+  export CLAM_FINISH_SOUND='/z/walrus/media/sounds/cartoons/meepmeep.wav'
+
+  # Setup for nethack adventure.
+  export NETHACKOPTIONS='name:Manjusri-W,dogname:Fred,catname:Zonker'
+
+  # mail setup for home machines.
+  export REPLYTO=fred@gruntose.com
+  export from="Fred T. Hamster <fred@gruntose.com>"
+
+  # set our browser for seti and others that use the variable.
+  export BROWSER=/usr/bin/firefox
+
+  # editor and other mixed settings...
+  export VISUAL="$(which gvim)"
+  if [ $? -ne 0 ]; then
+    export VISUAL="$(which vi)"
+  fi
+  export EDITOR="$(which vi)"
+  # the editors for revision control must wait while document is edited,
+  # so gvim and others launched to x window are not appropriate.
+  export GIT_EDITOR="$EDITOR"
+  export SVN_EDITOR="$EDITOR"
+
+  # this hideous mess is necessitated by our not having found the source of the
+  # settings yet.  we override a few colors that look bad on a dark background.
+  export LS_COLORS='no=00:fi=00:di=01;37:ln=00;36:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=41;33;01:ex=00;32:*.cmd=00;32:*.exe=01;32:*.com=01;32:*.bat=01;32:*.btm=01;32:*.dll=01;32:*.tar=00;31:*.tbz=00;31:*.tgz=00;35:*.rpm=00;33:*.deb=00;33:*.arj=00;31:*.taz=00;31:*.lzh=00;31:*.zip=00;35:*.zoo=00;31:*.z=00;31:*.Z=00;31:*.gz=00;35:*.bz2=00;31:*.tb2=00;31:*.tz2=00;31:*.tbz2=00;31:*.avi=01;35:*.bmp=01;35:*.fli=01;35:*.gif=01;35:*.jpg=01;35:*.jpeg=01;35:*.mng=01;35:*.mov=01;35:*.mpg=01;35:*.pcx=01;35:*.pbm=01;35:*.pgm=01;35:*.png=01;35:*.ppm=01;35:*.tga=01;35:*.tif=01;35:*.xbm=01;35:*.xpm=01;35:*.dl=01;35:*.gl=01;35:*.wmv=01;35:*.aiff=00;32:*.au=00;32:*.mid=00;32:*.mp3=00;32:*.ogg=00;32:*.voc=00;32:*.wav=00;32:'
+
+  # we set this to avoid paying for slow app signing on win32.
+  export NO_SIGNING=true
 fi
 
-# add a bunch of folders to the list for checkin & checkout.  these are
-# definitely personal, and some of them are fairly dated (the yeti and
-# hoople folders, for example).
-REPOSITORY_LIST+=" cloud gamesave hoople hoople1 hoople2 quartz web yeti xsede/xsede_tests xsede/code/cak0l/trunk"
-
-# point to our local certificate for ssh usage.
-export SVN_SSH="ssh -i $HOME/.ssh/id_dsa_sourceforge"
-
-# Error and success noises for CLAM.
-export CLAM_ERROR_SOUND='/z/walrus/media/sounds/effects/bwaaang.wav /z/walrus/media/sounds/cartoons/doh4.wav'
-export CLAM_FINISH_SOUND='/z/walrus/media/sounds/cartoons/meepmeep.wav'
-
-# Setup for nethack adventure.
-export NETHACKOPTIONS='name:Manjusri-W,dogname:Fred,catname:Zonker'
-
-# mail setup for home machines.
-export REPLYTO=fred@gruntose.com
-export from="Fred T. Hamster <fred@gruntose.com>"
-
-# set our browser for seti and others that use the variable.
-export BROWSER=/usr/bin/firefox
-
-# editor and other mixed settings...
-export VISUAL="$(which gvim)"
-if [ $? -ne 0 ]; then
-  export VISUAL="$(which vi)"
-fi
-export EDITOR="$(which vi)"
-# the editors for revision control must wait while document is edited,
-# so gvim and others launched to x window are not appropriate.
-export GIT_EDITOR="$EDITOR"
-export SVN_EDITOR="$EDITOR"
-  
-# this hideous mess is necessitated by our not having found the source of the
-# settings yet.  we override a few colors that look bad on a dark background.
-export LS_COLORS='no=00:fi=00:di=01;37:ln=00;36:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=41;33;01:ex=00;32:*.cmd=00;32:*.exe=01;32:*.com=01;32:*.bat=01;32:*.btm=01;32:*.dll=01;32:*.tar=00;31:*.tbz=00;31:*.tgz=00;35:*.rpm=00;33:*.deb=00;33:*.arj=00;31:*.taz=00;31:*.lzh=00;31:*.zip=00;35:*.zoo=00;31:*.z=00;31:*.Z=00;31:*.gz=00;35:*.bz2=00;31:*.tb2=00;31:*.tz2=00;31:*.tbz2=00;31:*.avi=01;35:*.bmp=01;35:*.fli=01;35:*.gif=01;35:*.jpg=01;35:*.jpeg=01;35:*.mng=01;35:*.mov=01;35:*.mpg=01;35:*.pcx=01;35:*.pbm=01;35:*.pgm=01;35:*.png=01;35:*.ppm=01;35:*.tga=01;35:*.tif=01;35:*.xbm=01;35:*.xpm=01;35:*.dl=01;35:*.gl=01;35:*.wmv=01;35:*.aiff=00;32:*.au=00;32:*.mid=00;32:*.mp3=00;32:*.ogg=00;32:*.voc=00;32:*.wav=00;32:'
-
-# we set this to avoid paying for slow app signing on win32.
-export NO_SIGNING=true
 
