d18cd8304c44b5a0eb7e8d4399685a924c6f51fb
Fred T. Hamster <fred@gruntose.com>
2012-03-11 16:41:28 -0400

    nicer formatting.

--- scripts/notes/info_overload_report.sh
+++ scripts/notes/info_overload_report.sh
@@ -4,32 +4,34 @@
 REPORT_FILE="$HOME/quartz/history_info_overload.txt"
 
 # notes are individual files of tasks, usually, although some are combined.
-note_depth=$(find ~/quartz/grunty_notes/ -type f -exec echo \"{}\" ';' | grep -v "\.svn" | grep -v "\.git" | wc -l)
+note_depth=$(find ~/quartz/grunty_notes/ -type f -exec echo \"{}\" ';' | grep -v "\.svn" | grep -v "\.git" | wc -l | tr -d ' ')
 
 # the list files are web documents with to-do lists.  individual items are marked with <li>.
-item_depth=$(find ~/quartz/grunty_notes/ -type f -iname "*.html" -exec grep "<li" "{}" ';' | wc -l)
+item_depth=$(find ~/quartz/grunty_notes/ -type f -iname "*.html" -exec grep "<li" "{}" ';' | wc -l | tr -d ' ')
 
 # projects are slightly more productive, ongoing things that are very active.
-project_depth=$(find ~/quartz/projects/ -type f -exec echo \"{}\" ';' | grep -v "\.svn" | grep -v "\.git" | wc -l)
+project_depth=$(find ~/quartz/projects/ -type f -exec echo \"{}\" ';' | grep -v "\.svn" | grep -v "\.git" | wc -l | tr -d ' ')
 
 # source examples need to be sucked into other places, other codebases.  they are not
 # supposed to pile up here.
-source_example_depth=$(find ~/quartz/example_source_code/ -type f -exec echo \"{}\" ';' | grep -v "\.svn" | grep -v "\.git" | wc -l)
+source_example_depth=$(find ~/quartz/example_source_code/ -type f -exec echo \"{}\" ';' | grep -v "\.svn" | grep -v "\.git" | wc -l | tr -d ' ')
 
 total_overload=$(($note_depth + $item_depth + $project_depth + $source_example_depth))
 
 report="\
-We have studied your information overload and find that there are:\n\
-  $note_depth note files\n\
-  $item_depth to-do list items\n\
-  $project_depth project files\n\
-  $source_example_depth source examples\n\
+\n\
+Current information overload consists of:\n\
+\n\
+  $note_depth\tnote files\n\
+  $item_depth\tto-do list items\n\
+  $project_depth\tproject files\n\
+  $source_example_depth\tsource examples\n\
   -------\n\
-  $total_overload total items\n\
+  $total_overload\ttotal items\n\
+\n\
 Gathered On: $(date)\n\
 \n\
-##############\n\
-"
+##############"
 
 echo -e "$report" | tee -a "$REPORT_FILE"
 
