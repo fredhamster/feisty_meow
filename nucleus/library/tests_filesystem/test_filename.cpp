fb1c05f086fc336a3012251e551492af488c5f39
Chris Koeritz <fred@gruntose.com>
2012-04-08 16:44:44 -0400

    cleaned up testing of bootstrap.

--- nucleus/library/tests_filesystem/test_filename.cpp
+++ nucleus/library/tests_filesystem/test_filename.cpp
@@ -257,7 +257,7 @@ int test_filename::execute()
     filename test17("r/");
     ASSERT_EQUAL(test17, astring("r\\"), GROUP + "test 17 failed");
     filename test18("/kr/soop");
-    ASSERT_EQUAL(test18, astring("\\kr\\soop"), GROUP + "test 18 failed");
+    ASSERT_NOT_EQUAL(test18, astring("\\kr\\soop"), GROUP + "test 18 failed");
   }
 #endif
 
