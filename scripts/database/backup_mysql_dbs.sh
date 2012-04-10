888688bd3bce9a3cf255f78a88e661d6cfa9a3fb
Chris Koeritz <fred@gruntose.com>
2012-04-10 14:41:22 -0400

    tossing useless wrong-headed script.  will do instead with a cron job and put that in as a cron example.

--- scripts/database/backup_mysql_dbs.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-source "$FEISTY_MEOW_SCRIPTS/core/functions.sh"
-
-temphost=$(hostname | sed -e 's/\([^.]*\)\..*/\1/')
-tar -czf /z/stuffing/archives/backup_dbs_${temphost}_$(date_stringer).tar.gz /home/archives/mysql_dbs/*
