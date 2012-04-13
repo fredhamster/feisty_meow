bba185d33bbd94ab371a39299840d89d751a330d
Chris Koeritz <fred@gruntose.com>
2012-04-13 14:42:41 -0400

    u

--- examples/custom_overrides/fred/build_cak0l.sh
+++ examples/custom_overrides/fred/build_cak0l.sh
@@ -4,6 +4,12 @@ function build_cak0l()
 {
   pushd ~/xsede/code/cak0l/trunk
   ant -Dbuild.targetArch=64 build
+  # fix memory limits.
+  sed -e "s/-Xmx512M/-Xmx2G/" < "runContainer.sh" >"$TMP/${USER}_edited.tmp"
+  mv "$TMP/${USER}_edited.tmp" "runContainer.sh" 
+  sed -e "s/-Xmx512M/-Xmx1G/" < "grid" >"$TMP/${USER}_edited.tmp"
+  mv "$TMP/${USER}_edited.tmp" "grid" 
+  chmod 755 "grid" "runContainer.sh" 
   popd
 }
 
