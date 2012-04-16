6b33e31e0542848d3bb19b30e46185807bf0fe19
Chris Koeritz <fred@gruntose.com>
2012-04-16 11:07:43 -0400

    fixed botched editing for builder functions.

--- examples/custom_overrides/fred/build_cak0l.sh
+++ examples/custom_overrides/fred/build_cak0l.sh
@@ -9,21 +9,23 @@ function build_cak0l()
   if [ -f runContainer.sh ]; then
     sed -e "s/-Xmx512M/-Xmx2G/" < "runContainer.sh" >"$TMP/${USER}_edited.tmp"
     mv "$TMP/${USER}_edited.tmp" "runContainer.sh" 
-    sed -e "s/-Xmx512M/-Xmx1G/" < "grid" >"$TMP/${USER}_edited.tmp"
+    chmod 755 "runContainer.sh" 
   fi
   if [ -f grid ]; then
+    sed -e "s/-Xmx512M/-Xmx1G/" < "grid" >"$TMP/${USER}_edited.tmp"
     mv "$TMP/${USER}_edited.tmp" "grid" 
-    chmod 755 "grid" "runContainer.sh" 
+    chmod 755 "grid"
     popd
   fi
   if [ -f runContainer.bat ]; then
     sed -e "s/-Xmx512M/-Xmx2G/" < "runContainer.bat" >"$TMP/${USER}_edited.tmp"
     mv "$TMP/${USER}_edited.tmp" "runContainer.bat" 
-    sed -e "s/-Xmx512M/-Xmx1G/" < "grid" >"$TMP/${USER}_edited.tmp"
+    chmod 755 "runContainer.bat" 
   fi
-  if [ -f grid ]; then
-    mv "$TMP/${USER}_edited.tmp" "grid" 
-    chmod 755 "grid" "runContainer.bat" 
+  if [ -f grid.bat ]; then
+    sed -e "s/-Xmx512M/-Xmx1G/" < "grid.bat" >"$TMP/${USER}_edited.tmp"
+    mv "$TMP/${USER}_edited.tmp" "grid.bat" 
+    chmod 755 "grid.bat"
     popd
   fi
 }
