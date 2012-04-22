406b30b96a1a3a80e9aecfe8ec5730a47d0af9d7
Chris Koeritz <fred@gruntose.com>
2012-04-22 12:42:46 -0400

    pseudo-standardizing some more filenames.

--- scripts/opensim/backup_opensim.sh
+++ scripts/opensim/backup_opensim.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 # a simple script that backs up the opensim database assets.
 
+#hmmm: need to parameterize for the database name and secrets file and all that.
+#      would be nice to have a block of opensim variables, perhaps an associative
+#      array of config chunks.
+
 source "$FEISTY_MEOW_SCRIPTS/core/functions.sh"
 
 host=$(echo $(hostname) | sed -e 's/\([^.]*\)\..*/\1/')
 
-bkupname="opensim_bkup_${host}_$(date_stringer).mysql_bkup"
+bkupname="backup-opensim_${host}_$(date_stringer).mysql_bkup"
 mysqldump -u griduser -p$(cat $HOME/.secrets/opensim_db_password.txt) opensim > "$bkupname"
 # note that the above assumes the database is called "opensim".  it might be
 # called opengrid instead, based on the setup procedure that was followed.
