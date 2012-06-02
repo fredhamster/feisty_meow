c172a816a820279291b2775198f2c8de5f0d4c5b
Chris Koeritz <fred@gruntose.com>
2012-06-02 17:43:41 -0400

    nice, gets the gpg-agent running so my passwords will be remembered during my login sessions.

---
+++ examples/custom_overrides/fred/gpg-daemon-launcher.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+##############
+# Name   : gpg-daemon-launcher
+# Author : Chris Koeritz
+# Rights : Copyright (C) 2012-$now by Feisty Meow Concerns, Ltd.
+##############
+# This script is free software; you can modify/redistribute it under the terms
+# of the GNU General Public License. [ http://www.gnu.org/licenses/gpl.html ]
+# Feel free to send updates to: [ fred@gruntose.com ]
+##############
+
+# starts up the gpg-agent, but only if it's not already running.
+
+if [ -z "$(psa gpg-agent)" ]; then
+  gpg-agent --daemon --enable-ssh-support --write-env-file "${HOME}/.gpg-agent-info" &>$TMP/zz_gpg-agent-daemon.log
+fi
+
+
+
