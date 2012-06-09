b8b92d9e231f7fc3ef6e655146a2d3bea3933740
Fred T. Hamster <fred@gruntose.com>
2012-06-09 16:50:41 -0400

    comments updated

--- nucleus/library/structures/static_memory_gremlin.cpp
+++ nucleus/library/structures/static_memory_gremlin.cpp
@@ -1,16 +1,14 @@
-/*****************************************************************************\
-*                                                                             *
-*  Name   : static_memory_gremlin                                             *
-*  Author : Chris Koeritz                                                     *
-*                                                                             *
-*******************************************************************************
-* Copyright (c) 2004-$now By Author.  This program is free software; you can  *
-* redistribute it and/or modify it under the terms of the GNU General Public  *
-* License as published by the Free Software Foundation; either version 2 of   *
-* the License or (at your option) any later version.  This is online at:      *
-*     http://www.fsf.org/copyleft/gpl.html                                    *
-* Please send any updates to: fred@gruntose.com                               *
-\*****************************************************************************/
+/*
+*  Name   : static_memory_gremlin
+*  Author : Chris Koeritz
+*
+* Copyright (c) 2004-$now By Author.  This program is free software; you can
+* redistribute it and/or modify it under the terms of the GNU General Public
+* License as published by the Free Software Foundation; either version 2 of
+* the License or (at your option) any later version.  This is online at:
+*     http://www.fsf.org/copyleft/gpl.html
+* Please send any updates to: fred@gruntose.com
+*/
 
 #include "static_memory_gremlin.h"
 
