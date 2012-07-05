f253cf33b5e8b9563915f93cb25d67c1a7d7a18f
Chris Koeritz <fred@gruntose.com>
2012-07-05 17:32:24 -0400

    added another port mapping.

--- scripts/security/khandroma_tunnels.sh
+++ scripts/security/khandroma_tunnels.sh
@@ -9,7 +9,7 @@ fi
 
 while true; do
   echo Connecting jenkins and vms at khandroma.
-  ssh -i $HOME/.ssh/id_dsa_fred -2 -N -v -L "*:5902:localhost:5902" -L "*:5909:localhost:5909" -L "*:5908:localhost:5908" -L "*:4040:localhost:8080" fred@khandroma.cs.virginia.edu
+  ssh -i $HOME/.ssh/id_dsa_fred -2 -N -v -L "*:5905:localhost:5905" -L "*:5902:localhost:5902" -L "*:5909:localhost:5909" -L "*:5908:localhost:5908" -L "*:4040:localhost:8080" fred@khandroma.cs.virginia.edu
   bash $FEISTY_MEOW_SCRIPTS/multimedia/sound_play.sh $soundfile &>/dev/null </dev/null &
   echo "Got dumped from tunnels; re-establishing connection."
   echo "Note: if you're being asked for a password, you haven't set up an RSA key yet."
