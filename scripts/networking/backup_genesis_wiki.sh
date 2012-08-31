5312f30f45b29f3b70dc9e1238c29ee8be5bb0d8
Chris Koeritz <fred@gruntose.com>
2012-08-31 08:32:55 -0400

    updated script.

--- scripts/networking/backup_genesis_wiki.sh
+++ scripts/networking/backup_genesis_wiki.sh
@@ -2,6 +2,6 @@
 
 date_string="$(date +"%Y_%b_%e_%H%M" | sed -e 's/ //g')"
 
-zip -r "$HOME/archives/genesis2_wiki_as_of_${date_string}" /web/genesis2.virginia.edu/doc/
+zip -r "$HOME/archives/genesis2_wiki_as_of_${date_string}" /web/genesis2.virginia.edu/doc/ -x "*/xcg_releases/*" -x "*/xsede_beta/*" -x "*/GenesisII*gz"
 
 
