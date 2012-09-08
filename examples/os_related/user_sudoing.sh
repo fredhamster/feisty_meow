3adadcfc1a15a0f9260ca13dbb30a3470890732a
Chris Koeritz <fred@gruntose.com>
2012-09-08 14:24:18 -0400

    a couple of new example files.  one is my java formatting style for eclipse, and the other is a technique for switching users once one is already running as sudo.

---
+++ examples/os_related/user_sudoing.sh
@@ -0,0 +1,13 @@
+
+# if this script is run as sudo, then at some point it may be useful to become another
+# user, in order to run something from within their context.  this is one way to do it
+# with a semi-interactive set of steps...
+sudo -u chronical bash <<eof
+echo hello this is \$USER
+echo we be in \$(pwd)
+echo "where you at?"
+eof
+
+# this can also be done by running a script with all those commands in it.
+sudo -u chronical bash /home/chronical/thing_to_run_as_chronical.sh
+
