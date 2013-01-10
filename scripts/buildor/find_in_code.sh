1440fb4390cbd3d081a4165a1c3e1185a7b455bf
Chris Koeritz <fred@gruntose.com>
2013-01-10 14:56:21 -0500

    fixed for names with spaces in them.

--- scripts/buildor/find_in_code.sh
+++ scripts/buildor/find_in_code.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 seek="$1"; shift
 
-find . -type f | grep -v '.svn' | grep -v '.git' | grep -v '.exe' | xargs grep -li "$seek" 
+find . -type f -exec echo "\"{}\"" ';' | grep -v '.svn' | grep -v '.git' | grep -v '.exe' | xargs grep -li "$seek" 
