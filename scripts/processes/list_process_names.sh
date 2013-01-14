6c50d90a24c823e34878034fa7ae211e235454ec
Chris Koeritz <fred@gruntose.com>
2013-01-14 10:52:35 -0500

    new script is handy; just lists process names for a particular user.  just use a user of "." if you want to list all users process names.

---
+++ scripts/processes/list_process_names.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# looks for process names for a particular user.  if no user is specified, then this
+# assumes we should look for the current user's processes.
+
+user="$1"
+if [ -z "$user" ]; then
+  user="$USER"
+fi
+
+ps wuax | grep $user | awk '{ print $11; }'
