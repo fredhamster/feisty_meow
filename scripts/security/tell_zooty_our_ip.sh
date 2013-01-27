211fb16a38602f29a9722e33f34b5cacfbb9cfd6
Chris Koeritz <fred@gruntose.com>
2013-01-27 12:12:47 -0500

    fixed docs and added missing exit.

--- scripts/security/tell_zooty_our_ip.sh
+++ scripts/security/tell_zooty_our_ip.sh
@@ -8,19 +8,19 @@ remote_user="$1"; shift
 local_user="$1"; shift
 
 if [ -z "$server" -o -z "$remote_user" -o -z "$local_user" ]; then
-  echo "This script will write a record of the IP address for the host where it is"
-  echo "run into a file in a remote computer.  To do this feat, it needs the"
-  echo "following parameters:"
+  echo "This script will record the IP address for 'this' host into a file on a"
+  echo "remote computer (that is running ssh server).  To perform this feat, the"
+  echo "following parameters are required:"
   echo "  $(basename $0) {server} {remote-user} {local-user}"
   echo "Note that this script must be run as root, but it uses the local user's"
   echo "capability to connect to the remote system without a password (given the"
   echo "user's possession of an ssh certificate on the remote host).  The remote"
   echo "user, in other words, must have an entry in the ssh authorized_keys that"
   echo "allows the local user to connect."
+  exit 1
 fi
 
 ip_file="$(hostname | sed -e "s/\..*$//")_ip_address.txt"
-echo ip file is $ip_file
 
 # go over to the server and write a file recording our IP address.
 # note that the local_user here is expected to have a certificate that
