a24e87eb5df257887924bd690796ce1f7fdab025
Chris Koeritz <fred@gruntose.com>
2013-02-12 15:26:13 -0500

    fixed inconsistent naming on change_endings, tossed out old script for finding bad protection on files, fixed up recursive renlower paths.

--- scripts/files/recursive_renlower.sh
+++ scripts/files/recursive_renlower.sh
@@ -2,6 +2,6 @@
 #need to support multiple names.
 for i; do
 #old  find "$1" -type d -depth -print -exec perl $FEISTY_MEOW_SCRIPTS/renlower.pl {}/* ';'
-  find "$i" -depth -exec perl $FEISTY_MEOW_SCRIPTS/renlower.pl "{}" ';'
+  find "$i" -depth -exec perl $FEISTY_MEOW_SCRIPTS/files/renlower.pl "{}" ';'
 done
 
