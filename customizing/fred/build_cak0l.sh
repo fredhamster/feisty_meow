e68a1cc658befa543b2c4d4238279e66210696b1
Chris Koeritz <fred@gruntose.com>
2013-04-01 13:10:32 -0400

    better version actually tests the pieces for success.

--- customizing/fred/build_cak0l.sh
+++ customizing/fred/build_cak0l.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+source "$FEISTY_MEOW_SCRIPTS/core/functions.sh"  # for check_result.
+
 function build_xsede()
 {
   pushd ~/xsede/code/cak0l/trunk
+  check_result "entering trunk directory"
   echo "Build starting at: $(date)"
   \rm -rf unit-test-reports
   ant -Dbuild.targetArch=64 build
+  check_result "building the trunk"
   # fix memory limits.
 #hmmm: clean these up.
   if [ -f runContainer.sh ]; then
@@ -32,6 +36,7 @@ function rebuild_xsede()
 {
   pushd ~/xsede/code/cak0l/trunk
   ant clean
+  check_result "cleaning the trunk"
   popd
   build_xsede
 }
