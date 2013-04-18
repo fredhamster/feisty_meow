b4d5edebb6afd504ae74fbbf31f2de338cc76002
Chris Koeritz <fred@gruntose.com>
2013-04-17 20:11:23 -0400

    nice mod to sort the files / dirs by name before dumping them.

---
+++ scripts/processes/show_hot_processes.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# shows the current processes ordered by cpu usage.
+
+# generate a nice separator.
+for ((i=0; i<14; i++)); do line+='='; done
+
+echo $line
+echo "Process list by CPU usage for $(date)"
+ps wuax --sort -%cpu  
+echo $line
