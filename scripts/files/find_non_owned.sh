74975aa423327346f2e2c62149e08570f22fd72d
Chris Koeritz <fred@gruntose.com>
2013-07-03 10:32:22 -0400

    added group check.

--- scripts/files/find_non_owned.sh
+++ scripts/files/find_non_owned.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 if test $# = 0; then
   echo $(basename $0): needs at least one directory name as a parameter.
+#hmmm: set the first parm to . instead.
   \exit 1;
 fi;
 export outfile="$(mktemp "$TMP/zz_findertmp.XXXXXX")"
-echo "These files are not self-owned:" >$outfile
+# check for files not owned by the user.
+echo "These files are not self-owned by $USER:" >$outfile
 for i; do
   find $i ! -user $USER >>$outfile
 done
-cat $outfile
-  # apparently this utility is supposed to echo to standard out.
+# check for files not in same group as the user.
+GROUP="$(groups | awk '{print $1}')"
+  # assumption above that the first group is the 'primary' one.
+echo "These files are owned by primary group of $GROUP:" >>$outfile
+for i; do
+  find $i ! -group $GROUP >>$outfile
+done
+
+less $outfile
+
 rm $outfile
 
