2a3a67a56b96e32b09e50e8ca9f29416fe42eff7
Chris Koeritz <fred@gruntose.com>
2013-07-24 17:39:28 -0500

    added NO_TEST_RUNS flag to skip running any tests.

--- scripts/clam/target_runner.sh
+++ scripts/clam/target_runner.sh
@@ -16,8 +16,9 @@
 #echo -e "\tDIRTY_FILE=${DIRTY_FILE}"
 #echo -e "\tSUBMAKE_FLAG=${SUBMAKE_FLAG}"
 #echo -e "\tFAILURE_FILE=${FAILURE_FILE}"
+#echo -e "\tNO_TEST_RUNS=${NO_TEST_RUNS}"
 
-if [ ! -z "${RUN_TARGETS}" ]; then
+if [ ! -z "${RUN_TARGETS}" -a -z "${NO_TEST_RUNS}" ]; then
   if [ -f "${DIRTY_FILE}" -o -f "${SUBMAKE_FLAG}" ]; then
     total_exitval=0;
     for program_name in ${RUN_TARGETS}; do
