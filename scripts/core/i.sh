bec09aa3c5c79cff7981b3972deebc9a040a0e68
Chris Koeritz <fred@gruntose.com>
2013-09-17 16:38:49 -0400

    updated to show lsb release info.

--- scripts/core/i.sh
+++ scripts/core/i.sh
@@ -6,7 +6,10 @@ source "$FEISTY_MEOW_SCRIPTS/core/functions.sh"
 echo '###############################################################################'
 echo
 echo Your user name is $USER on a computer named $(hostname).
-echo Your machine platform is $(uname -m).
+echo Your machine platform is $(uname -m)
+if [ $OPERATING_SYSTEM == "UNIX" ]; then
+  lsb_release -a
+fi
 echo The time is $(date_stringer | sed -e 's/_/ /g' | sed -e 's/\([0-9][0-9]\) \([0-9][0-9]\)$/:\1:\2/')
 echo
 echo '###############################################################################'
