e81f45c55fb9ac42e1ce57fc50fa3cfb2b407e3a
Chris Koeritz <fred@gruntose.com>
2013-10-08 19:15:05 -0400

    new script to resolve tree problem for svn.

---
+++ scripts/rev_control/fix_svn_item.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# resolves a tree conflict by accepting the "working" version,
+# which effectively makes your current change the accepted one.
+
+filename="$1"; shift
+
+svn resolve  --accept=working "$filename"
