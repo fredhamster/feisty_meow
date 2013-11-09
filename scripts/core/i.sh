218aff23f25eb65bf53f43daf9fc046049d57d1b
Chris Koeritz <fred@gruntose.com>
2013-11-09 11:33:09 -0500

    improved inventory command.

--- scripts/core/i.sh
+++ scripts/core/i.sh
@@ -2,8 +2,7 @@
 
 source "$FEISTY_MEOW_SCRIPTS/core/functions.sh"
 
-(
-echo '###############################################################################'
+echo '=============='
 echo
 echo Your user name is $USER on a computer named $(hostname).
 echo Your machine platform is $(uname -m)
@@ -15,20 +14,15 @@ if [ $OPERATING_SYSTEM == "UNIX" ]; then
 fi
 echo The time is $(date_stringer | sed -e 's/_/ /g' | sed -e 's/\([0-9][0-9]\) \([0-9][0-9]\)$/:\1:\2/')
 echo
-echo '###############################################################################'
+echo '=============='
 echo
 echo You have the following files here:
 ls -FC
 echo
-echo '###############################################################################'
+echo '=============='
 echo
 echo You are sharing the machine with:
 who
 echo
-echo '###############################################################################'
-echo
-echo You are running the following processes:
-. $FEISTY_MEOW_SCRIPTS/users/findme.sh
-echo
-echo '###############################################################################'
-) | less
+echo '=============='
+
