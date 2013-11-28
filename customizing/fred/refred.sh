4808a5b08e7dcb7c42ddb3e7595e8282b1c4eeb1
Chris Koeritz <fred@gruntose.com>
2013-11-27 20:05:30 -0500

    nice improvement, to move out the custom script so it can be run properly by root.  more sudo usage seems slightly safer.

---
+++ customizing/fred/refred.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# cleans up the ownership for all my files.
+function refred()
+{
+  chown -R fred:fred /home/fred /home/games /home/archives
+  bash $FEISTY_MEOW_SCRIPTS/files/normal_perm.sh /var/log
+}
+
+# this block should execute when the script is actually run, rather
+# than when it's just being sourced.
+if [[ $0 =~ .*refred\.sh.* ]]; then
+  THISDIR="$( \cd "$(\dirname "$0")" && \pwd )"
+  export LIGHTWEIGHT_INIT=true
+  source "$THISDIR/../../scripts/core/launch_feisty_meow.sh"
+  refred
+fi
+
