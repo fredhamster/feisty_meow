4f9dfa7a9756ac7619824845c07a936c73650ab2
Chris Koeritz <fred@gruntose.com>
2014-01-08 11:18:43 -0500

    cleaned up redundant sound playing script, adjusted clam to cope.

--- scripts/clam/exit_make.sh
+++ scripts/clam/exit_make.sh
@@ -3,6 +3,6 @@
 # than a fresh build next time.
 rm -f $FLAG_FILES $SUB_FLAG_FILES
 if [ ! -z "$CLAM_ERROR_SOUND" ]; then
-  bash $CLAM_DIR/sound_play.sh $CLAM_ERROR_SOUND
+  bash $FEISTY_MEOW_SCRIPTS/multimedia/sound_play.sh $CLAM_ERROR_SOUND
 fi
 exit 1;  # cause an error to stop the make.
