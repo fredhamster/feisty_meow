f223c759b20cd1820b30c9b12ce2bce55d29c842
Chris Koeritz <fred@gruntose.com>
2014-01-31 15:50:51 -0500

    renamed, made more general.

--- scripts/archival/zip_fred_xsede.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-source $FEISTY_MEOW_SCRIPTS/core/functions.sh
-
-zip -r $HOME/gffs_fred_code_backup_$(date_stringer).zip $HOME/xsede/code/fred/trunk/ --exclude ".svn" --exclude "bin.eclipse" --exclude "bin.ant" --exclude "codegen" --exclude "genned-src" --exclude "genned-obj" &>$TMP/zz_codebackup.log
