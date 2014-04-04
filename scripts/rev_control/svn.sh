533c009ada6c8302d76cf2e3c1179f7e1c8e981b
Chris Koeritz <fred@gruntose.com>
2014-04-04 10:29:02 -0400

    added script to fix cygwin issues with paths, where windows apps need a real TMP and cygwin barfs on same.

---
+++ scripts/rev_control/svn.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ "$OPERATING_SYSTEM" = "WIN32" ]; then
+  source $FEISTY_MEOW_SCRIPTS/core/functions.sh
+
+  TMP="$(dos_to_unix_path "$TMP")"
+fi
+
+echo TMP is $TMP 
+
+/usr/bin/svn $@
+
