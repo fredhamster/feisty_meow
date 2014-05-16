77891416345e714cd6b1be0828dfaf9e4f17987a
Chris Koeritz <fred@gruntose.com>
2014-05-16 15:06:07 -0400

    new fortune.  added missing sudo for game perm fixer.

--- scripts/files/fix_game_perms.sh
+++ scripts/files/fix_game_perms.sh
@@ -3,8 +3,8 @@
 # fixes the permissions of files in the local games directory so that
 # everyone can run or read them if the owner could.
 
-find /home/games -type f -perm /100 -exec chmod a+x {} ';'
-find /home/games -type f -perm /400 -exec chmod a+r {} ';'
+sudo find /home/games -type f -perm /100 -exec chmod a+x {} ';'
+sudo find /home/games -type f -perm /400 -exec chmod a+r {} ';'
 
-find /home/games -type d -exec chmod a+rx {} ';'
+sudo find /home/games -type d -exec chmod a+rx {} ';'
 
