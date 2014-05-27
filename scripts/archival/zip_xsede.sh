1c3ba86636291f75179d85dd76c732f546340f5a
Chris Koeritz <fred@gruntose.com>
2014-05-27 14:50:13 -0400

    really omitting .svn and other non-intended files now.

--- scripts/archival/zip_xsede.sh
+++ scripts/archival/zip_xsede.sh
@@ -2,4 +2,4 @@
 
 source $FEISTY_MEOW_SCRIPTS/core/functions.sh
 
-zip -r $HOME/gffs_backup_$(date_stringer).zip $GENII_INSTALL_DIR --exclude ".svn" --exclude "bin.eclipse" --exclude "bin.ant" --exclude "codegen" --exclude "genned-src" --exclude "genned-obj" &>$TMP/zz_codebackup.log
+zip -r $HOME/gffs_backup_$(date_stringer).zip $GENII_INSTALL_DIR --exclude "*/.svn/*" --exclude "*/bin.eclipse/*" --exclude "*/bin.ant/*" --exclude "*/codegen/*" --exclude "*/genned-src/*" --exclude "*/genned-obj/*" &>$TMP/zz_codebackup.log
