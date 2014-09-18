176dcb650f1dbefa08a342d83b4141ace594e05c
Chris Koeritz <fred@gruntose.com>
2014-09-18 10:03:23 -0400

    added exclusion for the installer packages of genesis2.

--- scripts/networking/backup_genesis_wiki.sh
+++ scripts/networking/backup_genesis_wiki.sh
@@ -2,6 +2,6 @@
 
 source "$FEISTY_MEOW_SCRIPTS/core/functions.sh"
 
-zip -r "/z/stuffing/archives/genesis2_wiki_as_of_$(date_stringer).zip" /z/uvaweb/doc/wiki -x "*/xcg_releases/*" -x "*/xsede_beta/*" -x "*/GenesisII*gz" -x "*/act126_installers/*"
+zip -r "/z/stuffing/archives/genesis2_wiki_as_of_$(date_stringer).zip" /z/uvaweb/doc/wiki -x "*/xcg_releases/*" -x "*/xsede_beta/*" -x "*/GenesisII*gz" -x "*/act126_installers/*" -x "*/genesis2-*"
 
 
