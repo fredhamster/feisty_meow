50a952e23d8fa60213b272b55309b633b923fa11
Chris Koeritz <fred@gruntose.com>
2015-02-01 21:53:59 -0500

    simple script that maybe will become useful in tracking down why some boots of a particular computer are great and others are characterized by very slow performance, super laggy audio, etc.

---
+++ scripts/system/show_diagnostics.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+source "$FEISTY_MEOW_SCRIPTS/core/functions.sh"
+
+echo '=============='
+echo $(date_stringer | sed -e 's/_/ /g' | sed -e 's/\([0-9][0-9]\) \([0-9][0-9]\)$/:\1:\2/')
+echo '=============='
+echo
+echo "Simple iostat:"
+echo
+iostat
+echo
+echo '=============='
+echo
+echo "Simple mpstat:"
+echo
+mpstat
+echo
+echo '=============='
+echo
+echo "Simple vmstat:"
+echo
+vmstat
+echo
+echo '=============='
+echo
+echo '=============='
+echo
+
