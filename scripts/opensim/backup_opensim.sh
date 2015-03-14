bb8cffeaab130cf52b8c5d4502c841c3eca71176
Fred T. Hamster <fred@gruntose.com>
2015-03-13 20:38:57 -0400

    update waiting here on wildmutt for a while.

--- scripts/opensim/backup_opensim.sh
+++ scripts/opensim/backup_opensim.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 # a simple script that backs up the opensim database assets.
 
+gridusername="$1"; shift
+databasename="$1"; shift
+
+if [ -z "$gridusername" ]; then
+  gridusername=griduser
+fi
+if [ -z "$databasename" ]; then
+  databasename=opensim
+fi
+
 #hmmm: need to parameterize for the database name and secrets file and all that.
 #      would be nice to have a block of opensim variables, perhaps an associative
 #      array of config chunks.
@@ -10,7 +20,7 @@ source "$FEISTY_MEOW_SCRIPTS/core/functions.sh"
 host=$(echo $(hostname) | sed -e 's/\([^.]*\)\..*/\1/')
 
 bkupname="backup-opensim_${host}_$(date_stringer).mysql_bkup"
-mysqldump -u griduser -p$(cat $HOME/.secrets/opensim_db_password.txt) opensim > "$bkupname"
+mysqldump -u "$gridusername" -p$(cat $HOME/.secrets/opensim_db_password.txt) "$databasename" > "$bkupname"
 # note that the above assumes the database is called "opensim".  it might be
 # called opengrid instead, based on the setup procedure that was followed.
 # likewise the user might be someone other than "griduser".
