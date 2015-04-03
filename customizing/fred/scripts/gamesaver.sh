3a7c19519e252577a7dbe781f99802dba8b0f70e
Chris Koeritz <fred@gruntose.com>
2015-04-03 08:08:40 -0400

    updated fnv info with barely readable info, hierarchicalized gamesaver.

--- customizing/fred/scripts/gamesaver.sh
+++ customizing/fred/scripts/gamesaver.sh
@@ -1,20 +1,20 @@
 
 # hmmm: some better work could be done to have both names (wineprefix and storage dir) be iterable.
 for i in oblivion fallout_new_vegas fallout_3 skyrim ; do
-  if [ ! -d $HOME/spooling_saves_$i ]; then
-    mkdir $HOME/spooling_saves_$i 
+  if [ ! -d $HOME/spooling_saves/$i ]; then
+    mkdir -p $HOME/spooling_saves/$i 
   fi
 done
 echo "======="
 echo skyrim
-cp -v -n ~/My\ Games/Skyrim/Saves/* ~/spooling_saves_skyrim
+cp -v -n ~/wine_goods/My\ Games/Skyrim/Saves/* ~/spooling_saves/skyrim
 echo "======="
 echo fallout new vegas
-cp -v -n ~/My\ Games/FalloutNV/Saves/* ~/spooling_saves_fallout_new_vegas/
+cp -v -n ~/wine_goods/My\ Games/FalloutNV/Saves/* ~/spooling_saves/fallout_new_vegas/
 echo "======="
 echo fallout 3
-cp -v -n ~/My\ Games/Fallout3/Saves/Player1/* ~/spooling_saves_fallout_3
+cp -v -n ~/wine_goods/My\ Games/Fallout3/Saves/Player1/* ~/spooling_saves/fallout_3
 echo "======="
 echo oblivion
-cp -v -n ~/My\ Games/Oblivion/Saves/* ~/spooling_saves_oblivion/
+cp -v -n ~/wine_goods/My\ Games/Oblivion/Saves/* ~/spooling_saves/oblivion/
 echo "======="
