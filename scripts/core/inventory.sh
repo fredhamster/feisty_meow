58d1629f8127a56d46c367214742b3db3097bad1
Chris Koeritz <fred@gruntose.com>
2015-04-10 18:19:21 -0400

    removed gaudy boxes in favor of shorter listing.

--- scripts/core/inventory.sh
+++ scripts/core/inventory.sh
@@ -5,9 +5,6 @@
 
 source "$FEISTY_MEOW_SCRIPTS/core/functions.sh"
 
-echo
-echo '++++++++++++++++++++++++++++++++++++++++++'
-
 unset codename
 if [ $OPERATING_SYSTEM == "UNIX" ]; then
   which lsb_release &>/dev/null
@@ -20,22 +17,20 @@ if [ -z "$codename" ]; then
 fi
 
 echo
-echo "it is $(date +"%A at %H%M hours on day%e of the %B moon in the gregorian year %Y" | tr A-Z a-z) and our intrepid adventurer $USER is exploring a computer named $(hostname) (code-name $codename) and has found that the machine's OS platform is $(uname -m) and its current incarnation has been $(uptime -p)." | splitter 
+echo "it is $(date +"%A at %H%M hours on day %e of the %B moon in the gregorian year %Y" | tr A-Z a-z) and our intrepid adventurer $USER is exploring a computer named $(hostname) (code-name $codename) and has found that the machine's OS platform is $(uname -m) and its current incarnation has been $(uptime -p)." | splitter 
 #hmmm: splitter not accepting these args properly right now:
 #--mincol 2 --maxcol 40
 echo
-echo '++++++++++++++++++++++++++++++++++++++++++'
-echo
+#echo '++++++++++++++++++++++++++++++++++++++++++'
+#echo
 echo "the following things appear to be lying around here..."
 echo
 ls -hFC $color_add
 echo
-echo '++++++++++++++++++++++++++++++++++++++++++'
-echo
+#echo '++++++++++++++++++++++++++++++++++++++++++'
+#echo
 echo "there appear to be these entities on this host..."
 echo
 who -suT
-echo
-echo '++++++++++++++++++++++++++++++++++++++++++'
 echo
 
