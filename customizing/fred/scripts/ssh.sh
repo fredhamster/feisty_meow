12c3bec1f9c8e45a893e13c7628ae530f483043a
Chris Koeritz <fred@gruntose.com>
2015-04-20 18:15:47 -0400

    added comment for future enhancement.

--- customizing/fred/scripts/ssh.sh
+++ customizing/fred/scripts/ssh.sh
@@ -14,6 +14,12 @@ fi
 # we don't want the remote side still thinking it's running xterm.
 export TERM=linux
 
+#hmmm: it would be good to set an interrupt handler here and
+#      trap ctrl-c, since otherwise we are getting exited from and losing a chance
+#      to reset the terminal title.  this actually happens a lot, since some X11
+#      or other background process is left running and the ssh never actually quits,
+#      forcing one to hit ctrl-c.
+
 if [ ! -z "$keyfile" ]; then
   \ssh -i "$keyfile" -X -C -c blowfish-cbc $*
 else
