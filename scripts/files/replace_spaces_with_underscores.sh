4bd5169eb75c769397e1c86069f9d2356c89d63d
Chris Koeritz <fred@gruntose.com>
2015-08-20 14:03:17 -0400

    stopped allowing ampersands in file names, added documentation lines.

--- scripts/files/replace_spaces_with_underscores.sh
+++ scripts/files/replace_spaces_with_underscores.sh
@@ -12,7 +12,11 @@ fi
 
 while [ $# -gt 0 ]; do
   file="$1"; shift
-  newname="$(echo "$file" | tr -s ' ' '_' | tr -d "\$\!@#%}{)(][\\\~',:?><\"" | sed -e 's/\([0-9]\)_\./\1./g' | sed -e 's/_-_/-/' )"
+  # first turn spaces into underscores.  then process characters we don't want
+  # in names.  then translate multiple underscores into just one.  then turn
+  # number followed by underscore into just number (?).  then translate
+  # underscore dash underscore into just dash.
+  newname="$(echo "$file" | tr -s ' ' '_' | tr -d "\$\!@&#%}{)(][\\\~',:?><\"" | sed -e 's/__/_/g' | sed -e 's/\([0-9]\)_\./\1./g' | sed -e 's/_-_/-/' )"
   if [ "$file" != "$newname" ]; then
     # we've effected a name change, so let's actually do it.
     echo "'$file' => '$newname'"
