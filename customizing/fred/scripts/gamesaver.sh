aca120f67d0ed05b3001c761007f7e3cec0b9c7f
Chris Koeritz <fred@gruntose.com>
2016-04-03 14:45:15 -0400

    made paths consistent.

--- customizing/fred/scripts/gamesaver.sh
+++ customizing/fred/scripts/gamesaver.sh
@@ -7,13 +7,13 @@ for i in oblivion fallout_new_vegas fallout_3 skyrim ; do
 done
 echo "======="
 echo skyrim
-cp -v -n ~/wine_goods/My\ Games/Skyrim/Saves/* ~/spooling_saves/skyrim
+cp -v -n ~/wine_goods/My\ Games/Skyrim/Saves/* ~/spooling_saves/skyrim/
 echo "======="
 echo fallout new vegas
 cp -v -n ~/wine_goods/My\ Games/FalloutNV/Saves/* ~/spooling_saves/fallout_new_vegas/
 echo "======="
 echo fallout 3
-cp -v -n ~/wine_goods/My\ Games/Fallout3/Saves/Player1/* ~/spooling_saves/fallout_3
+cp -v -n ~/wine_goods/My\ Games/Fallout3/Saves/Player1/* ~/spooling_saves/fallout_3/
 echo "======="
 echo oblivion
 cp -v -n ~/wine_goods/My\ Games/Oblivion/Saves/* ~/spooling_saves/oblivion/
