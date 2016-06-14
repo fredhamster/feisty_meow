29e6b182d3e9426d8e595e77cbbf6292e24b12a0
Chris Koeritz <fred@gruntose.com>
2016-06-14 11:58:55 -0400

    dropping a script that really is only needed at uva.

--- scripts/networking/backup_genesis_wiki.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-source "$FEISTY_MEOW_SCRIPTS/core/functions.sh"
-
-zip -r "/z/stuffing/archives/genesis2_wiki_as_of_$(date_stringer).zip" /z/uvaweb/doc/wiki -x "*/xcg_releases/*" -x "*/xsede_beta/*" -x "*/GenesisII*gz" -x "*/act126_installers/*" -x "*/genesis2-*" -x "*/udc3_releases/*" -x "*/xsede_releases/*" -x "*/gffseu_releases/*"
-
-
