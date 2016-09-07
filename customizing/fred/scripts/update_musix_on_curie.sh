45c5d720496075938c0b111e3afcf1fa38a2076e
Chris Koeritz <fred@gruntose.com>
2016-09-06 22:07:40 -0400

    another custom updater script.

---
+++ customizing/fred/scripts/update_musix_on_curie.sh
@@ -0,0 +1,8 @@
+
+#add the goodness around these like the nice updater.
+
+rsync -av /media/fred/fredmusicprime/musix/* /z/musix/
+rsync -avz /z/musix/* surya:/z/musix/ 
+rsync -avz /z/musix/* wildmutt:/z/musix/ 
+
+
