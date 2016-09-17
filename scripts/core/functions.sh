090458547273df6ef3b1ef4ef0ef54becdb2a63f
Chris Koeritz <fred@gruntose.com>
2016-09-17 18:26:08 -0400

    stuffed the recustomize function back in, since if it is a separate shell script it is executed in a bash subshell instead of directly in the current shell, which defeats the whole purpose of the macro being able to recustomize the current environment on the fly.

--- scripts/core/functions.sh
+++ scripts/core/functions.sh
@@ -11,6 +11,8 @@ if [ $? -eq 0 ]; then
     echo "skipping function definitions, because already defined."
   fi
   skip_all=yes
+else
+  skip_all=
 fi
 
 if [ -z "$skip_all" ]; then
@@ -74,7 +76,7 @@ if [ -z "$skip_all" ]; then
           echo "$varname=${!varname}"
         fi
       fi
-    done
+    done | sort
     IFS="$HOLDIFS"
   }
 
@@ -366,6 +368,44 @@ if [ -z "$skip_all" ]; then
     source $FEISTY_MEOW_SCRIPTS/core/launch_feisty_meow.sh
     # run nechung oracle to give user a new fortune.
     nechung
+  }
+
+  # copies a set of custom scripts into the proper location for feisty meow
+  # to merge their functions and aliases with the standard set.
+  function recustomize()
+  {
+    user="$1"; shift
+    if [ -z "$user" ]; then
+      # use our default example user if there was no name provided.
+      user=fred
+    fi
+    if [ ! -d "$FEISTY_MEOW_APEX/customizing/$user" ]; then
+      echo "The customization folder provided for $user should be:"
+      echo "  '$FEISTY_MEOW_APEX/customizing/$user'"
+      echo "but that folder does not exist.  Skipping customization."
+      return 1
+    fi
+    regenerate >/dev/null
+    pushd "$FEISTY_MEOW_LOADING_DOCK/custom" &>/dev/null
+    incongruous_files="$(bash "$FEISTY_MEOW_SCRIPTS/files/list_non_dupes.sh" "$FEISTY_MEOW_APEX/customizing/$user" "$FEISTY_MEOW_LOADING_DOCK/custom")"
+    
+    #echo "the incongruous files list is: $incongruous_files"
+    # disallow a single character result, since we get "*" as result when nothing exists yet.
+    if [ ${#incongruous_files} -ge 2 ]; then
+      echo "cleaning unknown older overrides..."
+      perl "$FEISTY_MEOW_SCRIPTS/files/safedel.pl" $incongruous_files
+      echo
+    fi
+    popd &>/dev/null
+    echo "copying custom overrides for $user"
+    mkdir -p "$FEISTY_MEOW_LOADING_DOCK/custom" 2>/dev/null
+    perl "$FEISTY_MEOW_SCRIPTS/text/cpdiff.pl" "$FEISTY_MEOW_APEX/customizing/$user" "$FEISTY_MEOW_LOADING_DOCK/custom"
+    if [ -d "$FEISTY_MEOW_APEX/customizing/$user/scripts" ]; then
+      echo "copying custom scripts for $user"
+      \cp -R "$FEISTY_MEOW_APEX/customizing/$user/scripts" "$FEISTY_MEOW_LOADING_DOCK/custom/"
+    fi
+    echo
+    regenerate
   }
 
   # generates a random password where the first parameter is the number of characters
