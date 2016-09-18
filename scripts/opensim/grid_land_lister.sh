d6b9b4e06ee566ba7c15039b479b2e95391d0e21
Chris Koeritz <fred@gruntose.com>
2016-09-17 20:07:05 -0400

    a few new scripts.  grid land lister does a dump of the uuids and such for land on my simulator; not very generic yet.  list old linux packs shows linux-image and linux-header that are in rc state, so not installed but also not clean.  rounder is a nice script or test runner that will keep going until a failure occurs; it keeps all the logs from each run in a directory under TMP.

---
+++ scripts/opensim/grid_land_lister.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# creates a file with the information about land on the
+# simulator.
+
+#hmmm: need to abstract the gridroot user, the output file name
+#(although support a default), and the database name.
+
+mysql -u gridroot -p > $HOME/dump_of_opensim_land_table.txt <<eof
+use wildmutt_opensim;
+select RegionUUID,Name,Description,OwnerUUID from land;
+eof
+
---
+++ scripts/system/list_old_linux_packages.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+bash $FEISTY_MEOW_SCRIPTS/system/list_packages.sh linux-image |grep "^rc" | grep -v extra | awk '{print $2 }'
+
+bash $FEISTY_MEOW_SCRIPTS/system/list_packages.sh linux-header |grep "^rc" | grep -v extra | awk '{print $2 }'
+
+
+
