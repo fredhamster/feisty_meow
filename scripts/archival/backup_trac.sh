b11fdd1305aa42bab8648bfae7c329eb49efbce9
Chris Koeritz <fred@gruntose.com>
2016-09-30 12:56:47 -0400

    updated to support a path for the archive locatin.

--- scripts/archival/backup_trac.sh
+++ scripts/archival/backup_trac.sh
@@ -3,9 +3,11 @@
 # backs up our trac repository.
 
 trac_path="$1"; shift
+archive_path="$1"; shift
 
-if [ -z "$trac_path" ]; then
-  echo This script needs the path to the trac database.
+if [ -z "$trac_path" -o -z "$archive_path" ]; then
+  echo This script needs the path to the trac database as the first parameter
+  echo and the path to the storage directory as the second parameter.
   exit 1
 fi
 
