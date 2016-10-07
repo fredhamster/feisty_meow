d32c47d2bdd2b136fdd46ed5dd38a39539d26730
Chris Koeritz <fred@gruntose.com>
2016-10-07 11:43:51 -0400

    dropped a couple of gffs backup scripts that i never used much.

--- scripts/archival/zip_gffs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-source $FEISTY_MEOW_SCRIPTS/core/functions.sh
-
-zip -r $HOME/gffs_backup_$(date_stringer).zip $GENII_INSTALL_DIR --exclude "*/.svn/*" --exclude "*/bin.eclipse/*" --exclude "*/bin.ant/*" --exclude "*/codegen/*" --exclude "*/genned-src/*" --exclude "*/genned-obj/*" &>$TMP/zz_codebackup.log
