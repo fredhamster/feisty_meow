94563cdf4ab63816d2d046fb69f0748a14f97554
Chris Koeritz <fred@gruntose.com>
2016-12-12 06:29:53 -0500

    fixed comments and got working
    nice changes while getting this new scheme working (thinking of downstream the right way).  left a documentation trail for next time when a relay repository needs to be created.

--- scripts/rev_control/push_repo_downstream.sh
+++ scripts/rev_control/push_repo_downstream.sh
@@ -5,14 +5,24 @@
 # the downstream destination might be a github or sourceforge repository that is
 # loaded from a personal repository or server.
 #
+# it is assumed that you have already added your ssh key to your github account.
+#
 # to set up the repository for relaying downstream, just do the normal checkout
 # or clone on it from the real origin.  for example:
 #
-# $ git clone git://feistymeow.org/feisty_meow
+# $ git clone git://feistymeow.org/feisty_meow feisty_relay
+#
+# change into that new directory:
+#
+# $ pushd feisty_relay
 #
 # and then add the downstream remote repository:
 #
-# $ git remote add downstream https://github.com/fredhamster/feisty_meow.git
+# # github example of add:
+# $ git remote add downstream git@github.com:fredhamster/feisty_meow.git
+#
+# # sourceforge example of add:
+# $ git remote add downstream ssh://fred_t_hamster@git.code.sf.net/p/feistymeow/trunk
 #
 # once the repository has been created, you can synch all updates that
 # have been checked into the origin repository with the downstream version
@@ -30,16 +40,16 @@ fi
 pushd "$dir"
 
 # get everything from the origin.
-git pull
+git fetch origin
 
 # get everything from the origin.
-git pull
+#needed? git pull
 
 # turn off occasionally troublesome setting before checkin.
 unset GIT_SSH
 
 # send the little boat down the stream to the dependent repository.
-git push origin master <"$PASSWORD_FILE"
+git push downstream master
 
 popd
 
