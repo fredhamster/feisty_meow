79797e49c9a83677f18fc4cec1ba949426adbfe7
Chris Koeritz <fred@gruntose.com>
2016-12-15 21:08:21 -0500

    dropped specialization for curie; now has space

--- customize/fred/scripts/synch_from_surya.sh
+++ customize/fred/scripts/synch_from_surya.sh
@@ -1,16 +1,6 @@
+#!/bin/bash
 
-# curie is the source for musix and also has limited space for walruses,
-# so we check for that being our host here.
-hostname -f 2>&1 | grep -i curie &>/dev/null
-if [ $? -eq 0 ]; then
-  # this name has curie in it, so we're probably on there.
-  echo "Updating with special case for curie from surya"
-  rsync -avz surya:/z/walrus/media/pictures/* /z/walrus/media/pictures/
-  rsync -avz surya:/z/walrus/media/sounds/* /z/walrus/media/sounds/
-else
-  echo "Updating standard host from surya"
-  rsync -avz surya:/z/walrus/* /z/walrus/
-  rsync -avz surya:/z/musix/* /z/musix/
-fi
-
+echo "Updating walrus and musix from surya"
+rsync -avz surya:/z/walrus/* /z/walrus/
+rsync -avz surya:/z/musix/* /z/musix/
 
