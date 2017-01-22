a8cba1c9db9eb124417e438720068d7d08cf36b3
Chris Koeritz <fred@gruntose.com>
2017-01-21 22:20:45 -0500

    dropping this for now; should use syncthing.

--- customize/fred/scripts/synch_from_surya.sh
+++ customize/fred/scripts/synch_from_surya.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-echo "Updating walrus and musix from surya"
-rsync -avz surya:/z/walrus/* /z/walrus/
-rsync -avz surya:/z/musix/* /z/musix/
+# echo "Updating walrus and musix from surya"
+# rsync -avz surya:/z/walrus/* /z/walrus/
+# rsync -avz surya:/z/musix/* /z/musix/
+
+
+echo this script is retired since syncthing is being investigated for this purpose.
+
+
 
