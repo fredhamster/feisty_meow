221dfeaf97ade253b3522f6b8405519a527fff77
Chris Koeritz <fred@gruntose.com>
2017-02-02 12:47:36 -0500

    another cleansing\
    getting through some more older things and tossing them perhaps.  also dropped a new folder since the files fit in other categories.

--- scripts/buildor/buildor_strip_code.sh
+++ scripts/buildor/buildor_strip_code.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source "$FEISTY_MEOW_SCRIPTS/build/seek_all_source.sh"
+source "$FEISTY_MEOW_SCRIPTS/buildor/seek_all_source.sh"
 
 function strip_file {
   file="$1"
--- scripts/buildor/zap_msbuilds.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# this simple script kills off some troublesome processes in preparation for a new build
-# with visual studio.
-zap_process.exe msbuild.exe 
-zap_process.exe mspdbsrv.exe
-
