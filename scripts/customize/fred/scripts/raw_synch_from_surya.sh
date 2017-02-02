450337ae1ed04f16ee6e4889532a0b497b34674a
Chris Koeritz <fred@gruntose.com>
2017-02-02 12:59:40 -0500

    and then any new adds...

---
+++ scripts/customize/fred/scripts/raw_synch_from_surya.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Updating walrus and musix from surya: raw mode without syncthing!"
+rsync -avz surya:/z/walrus/* /z/walrus/
+rsync -avz surya:/z/musix/* /z/musix/
+
---
+++ scripts/customize/fred/scripts/update_barkuptree.sh
@@ -0,0 +1,7 @@
+
+
+#hmmm: plug in good checking stuff from the updater for soapbox.
+
+rsync -av /z/pooling/archive_backups/* /media/fred/barkuptreedrive/archive_backups/
+rsync -av /z/walrus/* /media/fred/barkuptreedrive/walrus/ 
+rsync -av /z/musix/* /media/fred/barkuptreedrive/musix/
