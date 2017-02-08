b633fbb338874b684bb0879a92a012697e550adf
Fred T. Hamster <fred@gruntose.com>
2017-02-08 14:02:33 -0500

    barkuppier

--- scripts/customize/fred/scripts/update_barkuptree.sh
+++ scripts/customize/fred/scripts/update_barkuptree.sh
@@ -1,7 +1,13 @@
+#!/bin/bash
 
+# updates the mounted barkuptree drive with stuff on wildmutt.
+# very specific currently.
 
-#hmmm: plug in good checking stuff from the updater for soapbox.
+source "$FEISTY_MEOW_SCRIPTS/core/launch_feisty_meow.sh"
 
 rsync -av /z/pooling/archive_backups/* /media/fred/barkuptreedrive/archive_backups/
+check_result "synching archive backups"
 rsync -av /z/walrus/* /media/fred/barkuptreedrive/walrus/ 
+check_result "synching walrus"
 rsync -av /z/musix/* /media/fred/barkuptreedrive/musix/
+check_result "synching musix"
