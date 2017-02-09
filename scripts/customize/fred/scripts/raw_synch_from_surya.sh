3f5104d47ba8954aed0bbe4b0afd07b433c133b8
Chris Koeritz <fred@gruntose.com>
2017-02-09 09:39:52 -0500

    updated for new images folder

--- scripts/customize/fred/scripts/raw_synch_from_surya.sh
+++ scripts/customize/fred/scripts/raw_synch_from_surya.sh
@@ -3,4 +3,5 @@
 echo "Updating walrus and musix from surya: raw mode without syncthing!"
 rsync -avz surya:/z/walrus/* /z/walrus/
 rsync -avz surya:/z/musix/* /z/musix/
+rsync -avz surya:/z/imaginations/* /z/imaginations/
 
