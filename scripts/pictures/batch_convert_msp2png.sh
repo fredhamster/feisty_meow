36dfc6c0919590a9d4923a463616db044532b056
Chris Koeritz <fred@gruntose.com>
2017-02-09 07:35:03 -0500

    updated msp converters

---
+++ scripts/pictures/batch_convert_msp2png.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# converts ancient microsoft paint files to PNG format using recoil.
+# arose from a question on askubuntu: http://askubuntu.com/questions/881412/how-can-i-convert-really-old-files-in-microsoft-paint-format-msp-to-a-usabl
+# requires recoil to be installed: http://recoil.sourceforge.net/
+
+# run two phases since sometimes the ending is capitalized.
+for suffix in .msp .MSP; do
+  if [ ! -z "$(ls *${suffix} 2>/dev/null)" ]; then
+    for i in *${suffix} ; do recoil2png -o $(basename $i ${suffix}).png $i ; done
+  fi
+done
+
