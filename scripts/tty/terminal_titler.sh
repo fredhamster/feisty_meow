db68957e5a5356412268772335ac5771faec0ebe
Chris Koeritz <fred@gruntose.com>
2017-03-09 19:56:44 -0500

    terminal title back in black
    seems to now be totally working with much simpler mindset.  and the magical extra echo commands i needed are no longer needed, seemingly.  much simpler as a result.

--- scripts/tty/terminal_titler.sh
+++ scripts/tty/terminal_titler.sh
@@ -3,7 +3,8 @@
 source "$FEISTY_MEOW_SCRIPTS/core/functions.sh"
 source "$FEISTY_MEOW_SCRIPTS/core/common.alias"
 
-#export PRIOR_TERMINAL_TITLE
+# uncomment this to get extra noisy debugging.
+#export DEBUG_TERM_TITLE=true
 
 # puts a specific textual label on the terminal title bar.
 # this doesn't consider any previous titles; it just labels the terminal.
@@ -30,9 +31,6 @@ function apply_title_to_terminal()
 function set_terminal_title()
 {
   apply_title_to_terminal $*
-  # an echo or some interaction with terminal seems needed to get terminal to respond
-  # so we will see the current title when we query for it.
-  echo
   save_terminal_title
 }
 
@@ -46,10 +44,14 @@ function save_terminal_title()
     if [[ "$TERM" =~ .*"xterm".* && ! -z "$WINDOWID" ]]; then
       local prior_title="$(xprop -id $WINDOWID | perl -nle 'print $1 if /^WM_NAME.+= \"(.*)\"$/')"
       if [ ! -z "$prior_title" ]; then
-echo "saving prior terminal title as '$prior_title'"
+        if [ ! -z "$DEBUG_TERM_TITLE" ]; then
+          echo "saving prior terminal title as '$prior_title'"
+        fi
         export PRIOR_TERMINAL_TITLE="$prior_title"
       else
-echo "not saving prior terminal title which was empty"
+        if [ ! -z "$DEBUG_TERM_TITLE" ]; then
+          echo "not saving prior terminal title which was empty"
+        fi
       fi
     fi
   fi
@@ -64,7 +66,9 @@ function restore_terminal_title()
 
   # run the terminal labeller to restore the prior title, if there was one.
   if [ ! -z "$PRIOR_TERMINAL_TITLE" ]; then
-echo "restoring prior terminal title of '$PRIOR_TERMINAL_TITLE'"
+    if [ ! -z "$DEBUG_TERM_TITLE" ]; then
+      echo "restoring prior terminal title of '$PRIOR_TERMINAL_TITLE'"
+    fi
     apply_title_to_terminal "$PRIOR_TERMINAL_TITLE"
   fi
 }
@@ -74,7 +78,9 @@ function label_terminal_with_info()
 {
   # we only label the terminal anew if there's no saved title.
   if [ -z "$PRIOR_TERMINAL_TITLE" ]; then
-echo "showing new generated title since prior title was empty"
+    if [ ! -z "$DEBUG_TERM_TITLE" ]; then
+      echo "showing new generated title since prior title was empty"
+    fi
     pruned_host=$(echo $HOSTNAME | sed -e 's/^\([^\.]*\)\..*$/\1/')
     date_string=$(date +"%Y %b %e @ %T")
     user=$USER
@@ -84,13 +90,13 @@ echo "showing new generated title since prior title was empty"
     fi
     new_title="-- $user@$pruned_host -- [$date_string]"
     apply_title_to_terminal "$new_title"
-#needing this extra echo is awful.  is there a better way?
-    echo
     save_terminal_title
   else
     # use the former title; paste it back up there just in case.
-echo "showing prior terminal title since there was a prior title!"
-echo "using prior terminal title of '$PRIOR_TERMINAL_TITLE'"
+    if [ ! -z "$DEBUG_TERM_TITLE" ]; then
+      echo "showing prior terminal title since there was a prior title!"
+      echo "using prior terminal title of '$PRIOR_TERMINAL_TITLE'"
+    fi
     apply_title_to_terminal "$PRIOR_TERMINAL_TITLE"
   fi
 }
