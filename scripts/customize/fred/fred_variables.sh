5fd6f2cff4cae749812cebff2d0e4c6a842c12ee
Chris Koeritz <fred@gruntose.com>
2017-05-31 14:36:28 -0400

    muffed the folder name in repo list

--- scripts/customize/fred/fred_variables.sh
+++ scripts/customize/fred/fred_variables.sh
@@ -17,7 +17,7 @@ if [ -z "$USER_CUSTOMIZATIONS_LOADED" ]; then
   # add a bunch of folders to the list for checkin & checkout.  these are
   # definitely personal, and some of them are fairly dated (the yeti and
   # hoople folders, for example).
-  REPOSITORY_LIST+="cloud ebooks web projects/web_work"
+  REPOSITORY_LIST+="cloud ebooks web projects/webwork"
 
   # point to our local certificate for ssh usage.
   export SVN_SSH="ssh -i $HOME/.ssh/id_dsa_sourceforge"
