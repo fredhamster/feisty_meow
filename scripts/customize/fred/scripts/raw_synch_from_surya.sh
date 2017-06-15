69a3d67754820bf7232e1f4af6db064b63d1ff1f
Chris Koeritz <fred@gruntose.com>
2017-06-14 22:59:53 -0400

    added basement copy to script

--- scripts/customize/fred/scripts/raw_synch_from_surya.sh
+++ scripts/customize/fred/scripts/raw_synch_from_surya.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 echo "Updating walrus and musix from surya: raw mode without syncthing!"
-rsync -avz surya:/z/walrus/* /z/walrus/
-rsync -avz surya:/z/musix/* /z/musix/
+rsync -avz surya:/z/basement/* /z/basement/
 rsync -avz surya:/z/imaginations/* /z/imaginations/
+rsync -avz surya:/z/musix/* /z/musix/
+rsync -avz surya:/z/walrus/* /z/walrus/
 
