139878f9d2e62bb942812748bab565725e9f842d
Fred T. Hamster <fred@gruntose.com>
2017-10-08 13:16:00 -0400

    new folder being stored on barkuptree

--- scripts/customize/fred/scripts/update_barkuptree.sh
+++ scripts/customize/fred/scripts/update_barkuptree.sh
@@ -7,11 +7,20 @@ source "$FEISTY_MEOW_SCRIPTS/core/launch_feisty_meow.sh"
 
 export BARKY=/media/fred/barkuptreedrive
 
+# copy up the archived bluray discs, and possibly future archived formats.
 netcp /z/archons/* $BARKY/bkup_archons/
 check_result "synching archons"
+
+# copy over our somewhat attenuated but still important walrus archives.
 netcp /z/walrus/* $BARKY/walrus/ 
 check_result "synching walrus"
+
+# copy all the music files for future reference.
 netcp /z/musix/* $BARKY/musix/
 check_result "synching musix"
+
+# back up the photo archives.
+netcp /z/imaginations/* $BARKY/imaginations/
+check_result "synching imaginations"
 
 
