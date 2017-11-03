25005ff22c9f845ffffa682a903b0c0b8f76e28d
Chris Koeritz <fred@gruntose.com>
2017-11-03 00:06:59 -0400

    attempting to fix naive system updater
    the updater running from cron kept getting jammed up, with multiple possibly running at the same time.  this seems to be due to questions about using existing or new config files.  a fix suggested on internet says that dpkg needs to be told not to do that, and to just use the existing ones.

--- scripts/system/naive_system_updater.sh
+++ scripts/system/naive_system_updater.sh
@@ -3,7 +3,8 @@ source "$FEISTY_MEOW_SCRIPTS/core/launch_feisty_meow.sh"
 
 sudo apt-get update -y
 check_result "problem while doing 'apt-get update'"
-sudo apt-get dist-upgrade -y
+# new magic to tell dpkg to go with existing config files.  let's see if it works!
+sudo apt-get dist-upgrade -y -o Dpkg::Options::="--force-confold"
 check_result "problem while doing 'apt-get dist-upgrade'"
 
 
