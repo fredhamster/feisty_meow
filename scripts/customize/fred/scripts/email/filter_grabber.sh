911b4b90b5ec838dfc06e7746c2a7385ccd04067
Chris Koeritz <fred@gruntose.com>
2017-11-06 12:54:59 -0500

    new script for copying mail filters
    got tired of running these extra long commands.  totally specific to my one thunderbird config.

---
+++ scripts/customize/fred/scripts/email/filter_grabber.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# snags my two email accounts' mail filters into a nice storage location.
+
+source "$FEISTY_MEOW_SCRIPTS/core/launch_feisty_meow.sh"
+
+cp $HOME/.thunderbird/oqv30zg4.default/ImapMail/zooty.koeritz.com/msgFilterRules.dat $CLOUD_BASE/magic_cabinet/mail_filters/zooty_serene_hamstertronic_$(date_stringer).filters
+check_result "copying feistymeow.org filters"
+
+cp $HOME/.thunderbird/oqv30zg4.default/ImapMail/mail.eservices.virginia.edu/msgFilterRules.dat $CLOUD_BASE/magic_cabinet/mail_filters/uva_email_$(date_stringer).filters 
+check_result "copying UVa filters"
+
+
