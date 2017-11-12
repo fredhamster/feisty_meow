7aa39ad678cbcbdc638e03a2b0c50818eb8850f6
Chris Koeritz <fred@gruntose.com>
2017-11-12 02:37:34 -0500

    splitter magic for columns

--- scripts/core/inventory.sh
+++ scripts/core/inventory.sh
@@ -46,17 +46,11 @@ else
 fi
 echo
 echo "it is $(date +"%A at %H:%M hours on day %e of the %B moon in the gregorian year %Y" | tr A-Z a-z) and our intrepid adventurer $USER is exploring a computer named $(hostname) that is running in a thoughtspace called $osname $osver (code-name $codename), and $USER has deduced that the machine's OS platform is $(uname -m) and its current incarnation has been ${up}." | $splitter 
-#hmmm: splitter not accepting these args properly right now:
-#--mincol 2 --maxcol 40
 echo
-#echo '++++++++++++++++++++++++++++++++++++++++++'
-#echo
 echo "the following things appear to be lying around here..."
 echo
 ls -hFC $color_add
 echo
-#echo '++++++++++++++++++++++++++++++++++++++++++'
-#echo
 echo "there appear to be these entities on this host..."
 echo
 who -suT
