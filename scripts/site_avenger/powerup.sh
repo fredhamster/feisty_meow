7732a53caedd6e81d9f1872a5850a7b61c5562c5
Chris Koeritz <fred@gruntose.com>
2017-11-12 21:42:29 -0500

    trying a different way to drop perms

--- scripts/site_avenger/powerup.sh
+++ scripts/site_avenger/powerup.sh
@@ -27,14 +27,15 @@ export WORKDIR="$( \cd "$(\dirname "$0")" && \pwd )"  # obtain the script's work
 function print_instructions()
 {
   echo
-  echo "$(basename $0 .sh) [app dirname] [repository] [theme name] [user name]"
+  echo "$(basename $0 .sh) [app dirname] [repository] [theme name] "
+#[user name]
   echo
   echo "All parameters are optional, and intelligent guesses for them will be made."
   echo
   echo "app dirname: The folder where the app will be stored."
   echo "repository: The name of the git repository (short version, no URL)."
   echo "theme name: The name to use for the cakephp theme."
-  echo "user name: The name of the user to chown the checkout to."
+#  echo "user name: The name of the user to chown the checkout to."
   echo
   exit 0
 }
@@ -47,9 +48,9 @@ function print_instructions()
 app_dirname="$1"; shift
 repo_name="$1"; shift
 theme_name="$1"; shift
-user_name="$1"; shift
+#user_name="$1"; shift
 
-echo "*** user name is $user_name"
+#echo "*** user name is $user_name"
 
 if [ "$app_dirname" == "-help" -o "$app_dirname" == "--help" ]; then
   print_instructions
@@ -103,12 +104,12 @@ create_site_links "$site_store_path" "$theme_name"
 
 sep
 
-if [ ! -z "$user_name" ]; then
-  echo "Chowning the apps folder to be owned by: $user_name"
-#hmmm: have to hope for now for standard group named after user 
-  chown -R "$user_name:$user_name" "$APPLICATION_DIR"
-  test_or_die "Chowning $APPLICATION_DIR to be owned by $user_name"
-fi
+#if [ ! -z "$user_name" ]; then
+#  echo "Chowning the apps folder to be owned by: $user_name"
+##hmmm: have to hope for now for standard group named after user 
+#  chown -R "$user_name:$user_name" "$APPLICATION_DIR"
+#  test_or_die "Chowning $APPLICATION_DIR to be owned by $user_name"
+#fi
 
 sep
 
