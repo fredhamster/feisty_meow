d4c3a3cc7ed18ab37bdc4d952fd91b8a25a3da6b
Chris Koeritz <fred@gruntose.com>
2017-11-14 22:55:37 -0500

    much closer but chomper not right yet

--- scripts/system/common_sysadmin.sh
+++ scripts/system/common_sysadmin.sh
@@ -12,7 +12,7 @@ function remove_domain_file()
   local domain_file="/etc/bind/${domain_name}.conf"
   if [ -f "$domain_file" ]; then
     # don't destroy, just shuffle.
-    \mv -f "$domain_file" "/tmp/${domain_file}-old-${RANDOM}"
+    \mv -f "$domain_file" "/tmp/$(basename ${domain_file})-old-${RANDOM}"
     test_or_die "removing domain file: $domain_file"
   fi
 }
@@ -58,7 +58,7 @@ ${domain_name}.	IN A	${IP_ADDRESS}
   test_or_die "setting ownership on: $domain_file"
 }
 
-#hmmm: move to core!
+#hmmm: move this chomper to core!
 
 # given a filename and a string to seek and a number of lines, then this
 # function will remove the first occurrence of a line in the file that
@@ -69,12 +69,15 @@ function create_chomped_copy_of_file()
   local seeker="$1"; shift
   local numlines=$1; shift
 
+echo into create_chomped_copy...
+var filename seeker numlines 
+
   # make a backup first, oy.
   \cp -f "$filename" "$filename.bkup-${RANDOM}" 
   test_or_die "backing up file: $filename"
 
   # make a temp file to write to before we move file into place in bind.
-  local new_version="/tmp/${filename}.bkup-${RANDOM}" 
+  local new_version="/tmp/$(basename ${filename}).bkup-${RANDOM}" 
   \rm -f "$new_version"
   test_or_die "cleaning out new version of file from: $new_version"
 
@@ -84,19 +87,19 @@ function create_chomped_copy_of_file()
     # don't bother looking at the lines if we're already in skip mode.
     if [[ $skip_count == 0 ]]; then
       # find the string they're seeking.
-      if [[ ! "$line" =~ *"$seeker"* ]]; then
+      if [[ ! $line =~ *${seeker}* ]]; then
         # no match.
         echo "$line" >> "$new_version"
       else
         # a match!  start skipping.  we will delete this line and the next N lines.
-        ((skip_count++))
+        $((skip_count++))
 echo first skip count is now $skip_count
       fi
     else
       # we're already skipping.  let's keep going until we hit the limit.
-      ((skip_count++))
+      $((skip_count++))
 echo ongoing skip count is now $skip_count
-      if [[ $skip_count >= $numlines ]]; then
+      if (( $skip_count >= $numlines )); then
         echo "Done skipping, and back to writing output file."
         skip_count=0
       fi
@@ -113,7 +116,6 @@ exit 1
   \mv "$new_version" "$filename"
   test_or_die "moving the new version into place in: $filename"
 
-  
 }
 
 # takes a zone back out of the local conf file for bind
@@ -125,49 +127,48 @@ function remove_zone_for_domain()
 
   # eat the zone file definition.  this will botch up badly if more text was added
   # or the zone info shrank.
-  create_chomped_copy_of_file "$domain_file" "zone \"${domain_name}\"" 6
+  create_chomped_copy_of_file "/etc/bind/named.conf.local" "zone*${domain_name}" 6
 
-
-  \cp -f "$domain_file" "$domain_file.bkup-${RANDOM}" 
-  test_or_die "backing up domain file: $domain_file"
-
-  # temp file to write to before we move file into place in bind.
-  local new_version="/tmp/$domain_file.bkup-${RANDOM}" 
-  \rm -f "$new_version"
-  test_or_die "cleaning out new version of domain file from: $new_version"
-
-  local line
-  local skip_count=0
-  while read line; do
-    # don't bother looking at the lines if we're already in skip mode.
-    if [[ $skip_count == 0 ]]; then
-      # find the zone for the domain.
-      if [[ ! "$line" =~ *"zone \"${domain_name}\""* ]]; then
-        echo "$line" >> "$new_version"
-      else
-        # start skipping.  we will delete this line and the next 6 lines.
-        ((skip_count++))
-echo first skip count is now $skip_count
-      fi
-    else
-      # we're already skipping.  let's keep going until we hit the limit.
-      ((skip_count++))
-      if [[ $skip_count >= 6 ]]; then
-        echo "Done skipping, and back to writing output file."
-        skip_count=0
-      fi
-    fi
-  done < "$domain_file"
-
-#put the file back into place.
-echo file we created looks like this:
-filedump "$new_version"
-
-echo bailing
-exit 1
-
-  \mv "$new_version" "$domain_file"
-  test_or_die "moving the new version into place in: $domain_file"
+#  \cp -f "$domain_file" "$domain_file.bkup-${RANDOM}" 
+#  test_or_die "backing up domain file: $domain_file"
+#
+#  # temp file to write to before we move file into place in bind.
+#  local new_version="/tmp/$domain_file.bkup-${RANDOM}" 
+#  \rm -f "$new_version"
+#  test_or_die "cleaning out new version of domain file from: $new_version"
+#
+#  local line
+#  local skip_count=0
+#  while read line; do
+#    # don't bother looking at the lines if we're already in skip mode.
+#    if [[ $skip_count == 0 ]]; then
+#      # find the zone for the domain.
+#      if [[ ! "$line" =~ *"zone \"${domain_name}\""* ]]; then
+#        echo "$line" >> "$new_version"
+#      else
+#        # start skipping.  we will delete this line and the next 6 lines.
+#        ((skip_count++))
+#echo first skip count is now $skip_count
+#      fi
+#    else
+#      # we're already skipping.  let's keep going until we hit the limit.
+#      ((skip_count++))
+#      if [[ $skip_count >= 6 ]]; then
+#        echo "Done skipping, and back to writing output file."
+#        skip_count=0
+#      fi
+#    fi
+#  done < "$domain_file"
+#
+##put the file back into place.
+#echo file we created looks like this:
+#filedump "$new_version"
+#
+#echo bailing
+#exit 1
+#
+#  \mv "$new_version" "$domain_file"
+#  test_or_die "moving the new version into place in: $domain_file"
 
 }
 
@@ -196,7 +197,39 @@ zone \"${domain_name}\" in {
   # keep ownership for the real user.
   chown "$(logname):$(logname)" /etc/bind/named.conf.local
   test_or_die "setting ownership on: /etc/bind/named.conf.local"
+}
 
+# zaps a subdomain out of the containing domain file.
+function remove_subdomain()
+{
+  local old_domain="$1"; shift
+
+  # split up the full domain name into subdomain portion and containing domain.
+  local subdomain="${old_domain%.*.*}"
+  local containing_domain="${old_domain#*.}"
+
+  echo "removing subdomain $subdomain from containing domain $containing_domain"
+#hmmm: other functions could use that level of clarity in their logging.
+
+  local domain_file="/etc/bind/${containing_domain}.conf"
+  # see if config file already exists; if not, complain.
+  if [ ! -f "$domain_file" ]; then
+    echo "The domain configuration file for $old_domain is missing."
+    echo "It should already be present in: $domain_file"
+    echo "We cannot remove a subdomain if the containing domain isn't there."
+    exit 1
+  fi
+
+  # see if subdomain already present in config.
+  if [ ! $(grep -q "$old_domain" "$domain_file") ]; then
+    echo "The subdomain $subdomain is already missing from the domain"
+    echo "configuration file: $domain_file"
+    echo "Our work is apparently done for removing it."
+    return 0
+  fi
+
+  create_chomped_copy_of_file "$domain_file" \
+      "${subdomain}*${containing_domain}*IN*A*${IP_ADDRESS}" 1
 }
 
 # adds a new subdomain under a containing domain.
