340607dacaa38fe30cb5bf59d60c122f314e5ba5
Chris Koeritz <fred@gruntose.com>
2017-11-14 19:04:15 -0500

    first bit of tricky code
    this plunks the latest hello.txt file into place when the user revamps the cakelampvm

--- scripts/site_avenger/revamp_cakelampvm.sh
+++ scripts/site_avenger/revamp_cakelampvm.sh
@@ -20,28 +20,48 @@ source "$FEISTY_MEOW_APEX/scripts/core/launch_feisty_meow.sh"
 
 # fix up the main web storage.
 chown -R www-data:www-data /var/www 
+test_or_die "chown www-data"
 group_perm /var/www 
+test_or_die "group_perm www-data"
 
 ##############
 
 # set up access on some important folders for the developer user.
 chown -R developer:developer /home/developer
+test_or_die "chown developer home"
 harsh_perm /home/developer/.ssh
+test_or_die "harsh_perm setting on developer .ssh"
 chown -R developer:developer /etc/apache2 /etc/bind 
+test_or_die "chown apache2 and bind to developer"
 group_perm /etc/apache2 /etc/bind 
+test_or_die "group perms on apache2 and bind"
 
 ##############
 
 # fix perms for fred user.
 chown -R fred:fred /home/fred /home/archives/stuffing 
+test_or_die "chown fred home"
 harsh_perm /home/fred/.ssh
+test_or_die "harsh_perm setting on fred .ssh"
 chown -R fred:fred /opt/feistymeow.org 
+test_or_die "chown feisty meow to fred"
 group_perm /opt/feistymeow.org 
+test_or_die "group perms on feisty meow"
+
+##############
+#
+# some slightly tricky bits start here.  we want to massage the vm into the
+# best possible shape without needing to re-release it.
+#
+##############
+
+# copy the most recent hello file into place for the user.
+cp "$FEISTY_MEOW_APEX/production/sites/cakelampvm.com/hello.txt" "$HOME"
+test_or_continue "copying hello file for user"
 
 ##############
 
 #hmmm: todo
 # deploy the site updater here to fix the local cakelampvm.com site...
-
 
 
