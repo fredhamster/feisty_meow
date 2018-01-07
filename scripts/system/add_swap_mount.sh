320422572c26c1ddfbc73e6c6f64d370bacc72da
Chris Koeritz <fred@gruntose.com>
2018-01-07 10:53:51 -0500

    repair on sudo behavior

--- scripts/system/add_swap_mount.sh
+++ scripts/system/add_swap_mount.sh
@@ -2,7 +2,8 @@
 
 # auto-find the scripts, since we might want to run this as sudo.
 export WORKDIR="$( \cd "$(\dirname "$0")" && /bin/pwd )"  # obtain the script's working directory.
-source "$WORKDIR/../core/launch_feisty_meow.sh"
+export FEISTY_MEOW_APEX="$( \cd "$WORKDIR/../.." && \pwd )"
+source "$FEISTY_MEOW_APEX/scripts/core/launch_feisty_meow.sh"
 
 if [[ $EUID != 0 ]]; then
   echo "This script must be run as root or sudo."
@@ -21,7 +22,7 @@ swap memory.  This increases the amount of available memory on RAM constrained
 systems.  It accepts two parameters: (1) the instance of the swap file, which
 should be a small number not already used for a swap partition, and (2) the
 size of the swap file in megabytes."
-  exit 1
+  exit 0
 fi
 
 # if the swap instance variable is already set, then we'll use it.
