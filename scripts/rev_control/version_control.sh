f4a70f561331ef268496f0e198b778ecc8739573
Chris Koeritz <fred@gruntose.com>
2018-01-07 12:15:26 -0500

    another attempt to clean up superfluous merges
    we want to stop seeing pointless merge messages in the commit log.  this should take care of it, although there can be a problem if the code stashed before pulling is *way* out of synch with the code being pulled.  that problem would afflict the merge also though, and it is better to find out as soon as possible rather than letting the snarl grow larger.

--- scripts/rev_control/version_control.sh
+++ scripts/rev_control/version_control.sh
@@ -394,7 +394,7 @@ function do_careful_git_update()
     remote_branch_info=$(git ls-remote --heads origin $bran 2>/dev/null)
     if [ ! -z "$remote_branch_info" ]; then
       # we are pretty sure the remote branch does exist.
-      git pull origin "$bran" | $TO_SPLITTER
+      git pull --rebase --autostash origin "$bran" | $TO_SPLITTER
 # we may want to choose to do fast forward, to avoid crazy multiple merge histories
 # without any changes in them.  --no-ff
       promote_pipe_return 0
@@ -408,7 +408,7 @@ function do_careful_git_update()
 
   # now pull down any changes in our own origin in the repo, to stay in synch
   # with any changes from others.
-  git pull --all | $TO_SPLITTER
+  git pull --rebase --autostash --all | $TO_SPLITTER
 #is the above really important when we did this branch already in the loop?
 #it does an --all, but is that effective or different?  should we be doing that in above loop?
 # --no-ff   
@@ -450,7 +450,7 @@ function do_update()
   elif [ -d ".git" ]; then
     if test_writeable ".git"; then
       $blatt
-      git pull 2>&1 | grep -v "X11 forwarding request failed" | $TO_SPLITTER
+      git pull --rebase --autostash 2>&1 | grep -v "X11 forwarding request failed" | $TO_SPLITTER
 #ordinary pulls should be allowed to do fast forward: --no-ff 
       promote_pipe_return 0
       test_or_die "git pull of origin"
