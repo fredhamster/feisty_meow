77aaf89edd254173d7750fb1465130fa1431e98f
Chris Koeritz <fred@gruntose.com>
2018-02-23 21:49:08 -0500

    next try to fix upgrade stalling
    a new variable plus an extra force-confdef flag.

--- scripts/system/naive_system_updater.sh
+++ scripts/system/naive_system_updater.sh
@@ -3,8 +3,9 @@ source "$FEISTY_MEOW_SCRIPTS/core/launch_feisty_meow.sh"
 
 sudo apt-get update -y
 test_or_die "problem while doing 'apt-get update'"
-# new magic to tell dpkg to go with existing config files.  let's see if it works!
-sudo apt-get dist-upgrade -y -o Dpkg::Options::="--force-confold"
+# newest magic to tell dpkg to go with existing config files and force non-interactive version.
+export DEBIAN_FRONTEND=noninteractive
+sudo apt-get dist-upgrade -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"
 test_or_die "problem while doing 'apt-get dist-upgrade'"
 
 
