9ec7a5da5c1dfc0ca264e2e166f2c19bcf4a51b2
Chris Koeritz <fred@gruntose.com>
2018-03-20 21:49:31 -0400

    more accurate solution for non-interactive update
    should work now, if the reference sites are right about the variable for non-interactive.  the var was not passed in before since sudo is not like a sub-shell.  now the two pieces needed, setting the var and running command that listens to var, are done in a single multi-line bash command.

--- scripts/system/naive_system_updater.sh
+++ scripts/system/naive_system_updater.sh
@@ -3,9 +3,13 @@ source "$FEISTY_MEOW_SCRIPTS/core/launch_feisty_meow.sh"
 
 sudo apt-get update -y
 test_or_die "problem while doing 'apt-get update'"
+
 # newest magic to tell dpkg to go with existing config files and force non-interactive version.
-export DEBIAN_FRONTEND=noninteractive
-sudo apt-get dist-upgrade -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"
+sudo bash -c "\
+  export DEBIAN_FRONTEND=noninteractive; \
+  apt-get dist-upgrade -y -o Dpkg::Options::=\"--force-confdef\" \
+     -o Dpkg::Options::=\"--force-confold\"; \
+"
 test_or_die "problem while doing 'apt-get dist-upgrade'"
 
 
