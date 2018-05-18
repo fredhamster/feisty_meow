b88291228a119aad677528c664205844b6c4ec17
Chris Koeritz <fred@gruntose.com>
2018-05-18 12:27:11 -0400

    nice change for wildcard display
    used to fail if no package name was provided but now treats that as a wildcarding opportunity to show all packages.

--- scripts/system/list_packages.sh
+++ scripts/system/list_packages.sh
@@ -2,21 +2,30 @@
 
 source "$FEISTY_MEOW_SCRIPTS/core/launch_feisty_meow.sh"
 
+if [ -z "$*" ]; then
+  # no parameters, so we do a wildcard style of listing packages.
+  SEEK_PIECE=' cat '
+else
+  # we know the parameters are not empty, so we don't go with our
+  # wildcard approach for listing the package names.
+  SEEK_PIECE=' grep $* '
+fi
+
 if debian_like; then
-  dpkg -la | grep $*
+  dpkg -la | eval $SEEK_PIECE
   exit $?
 fi
 
 rpm_available="$(which rpm)"
 if [ ! -z "$rpm_available" ]; then
 #is that the right phrase for rpm?  somewhat forgotten.
-  rpm -qa | grep $*
+  rpm -qa | eval $SEEK_PIECE
   exit $?
 fi
 
 yum_available="$(which yum)"
 if [ ! -z "$yum_available" ]; then
-  yum list | grep $*
+  yum list | eval $SEEK_PIECE
   exit $?
 fi
 
