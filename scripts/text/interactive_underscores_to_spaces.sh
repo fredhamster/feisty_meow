d92821b46533210cd43b2ad35c093874f286b428
Chris Koeritz <fred@gruntose.com>
2018-05-25 18:11:13 -0400

    special purpose underscore to space converter
    just takes lines of input and turns the underscores back into spaces; part of a simple readability toolset for converting file names to titles.

---
+++ scripts/text/interactive_underscores_to_spaces.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+source "$FEISTY_MEOW_SCRIPTS/core/launch_feisty_meow.sh"
+
+while true; do
+  sep 14 
+  read line 
+  echo "< arfle_soiwe_pestunius" 
+  echo "> arfle_soiwe_pestunius" | sed -e 's/_/ /g'
+done
+
