f85c9c24b112ca45556fc424361101f1a9baa728
Chris Koeritz <fred@gruntose.com>
2019-01-22 22:58:07 -0500

    added toaster

--- scripts/customize/fred/scripts/archival/raw_synch.sh
+++ scripts/customize/fred/scripts/archival/raw_synch.sh
@@ -11,10 +11,12 @@ fi
 echo "Updating our local archives from $synch_host: this is very raw mode, without syncthing!" | splitter
 echo
 
-for currdir in basement imaginations musix walrus; do
-  sep
-  echo "synching $currdir folder from $synch_host..."
-  rsync -avz "$synch_host":/z/$currdir/* /z/$currdir/
+for currdir in basement imaginations musix toaster walrus; do
+  if [ -d /z/$currdir ]; then
+    sep
+    echo "synching $currdir folder from $synch_host..."
+    rsync -avz "$synch_host":/z/$currdir/* /z/$currdir/
+  fi
 done
 
 sep
