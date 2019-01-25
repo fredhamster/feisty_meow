03f97b57e9e4f0ce57d38d9b501f6e5b4502e93d
Chris Koeritz <fred@gruntose.com>
2019-01-25 03:43:58 -0500

    renamed for proper user

--- scripts/customize/dirt/scripts/redirt.sh
+++ scripts/customize/dirt/scripts/redirt.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-function do_redoug()
+function do_redirt()
 {
-  reapply_cool_permissions doug
+  reapply_cool_permissions dirt
 
-  # anything else specific to doug?
+  # anything else specific to dirt?
 }
 
 # this block should execute when the script is actually run, rather
 # than when it's just being sourced.
-if [[ $0 =~ .*redoug\.sh.* ]]; then
+if [[ $0 =~ .*redirt\.sh.* ]]; then
   source "$FEISTY_MEOW_SCRIPTS/core/launch_feisty_meow.sh"
   exit_on_error "sourcing the feisty meow environment"
   source "$FEISTY_MEOW_SCRIPTS/security/cool_permissionator.sh"
   exit_on_error "sourcing the permission script"
-  do_redoug
-  continue_on_error "redouging process"
+  do_redirt
+  continue_on_error "redirting process"
 fi
 
