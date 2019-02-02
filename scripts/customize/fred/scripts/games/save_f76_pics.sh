ef338226ccf699c8d8a149d9b74e9888175c7099
Chris Koeritz <fred@gruntose.com>
2019-02-02 17:20:58 -0500

    adapted to new storage location

--- scripts/customize/fred/scripts/games/save_f76_pics.sh
+++ scripts/customize/fred/scripts/games/save_f76_pics.sh
@@ -1,5 +1,5 @@
 
 source "$FEISTY_MEOW_SCRIPTS/core/launch_feisty_meow.sh"
 
-netcp ~/linx/wind_goods/My\ Games/Fallout\ 76/Photos/8f99c06443f04f6f8270604369bb78eb/*[0-9].png ~/data/f76_pc_pics/
+netcp ~/linx/wind_goods/My\ Games/Fallout\ 76/Photos/8f99c06443f04f6f8270604369bb78eb/*[0-9].png /z/walrus/media/pictures/metaverse/fallout_76/spoonburg_pc/
 
