467de4600d98554d4baab015630e8fbd7cafd0b0
Chris Koeritz <fred@gruntose.com>
2019-03-18 18:52:51 -0400

    working vendor record plus new system time update
    the vendor record got purged of its insane missing line problem by uploading it to google and then downloading it again.  freaking weird.
    the update_system_time script is a simple wrapper for rdate, which has been throwing obnoxious "problem: successful" kinds of messages for a while.  don't know if the weird success message is accompanied by a result value of zero, as in success, or not, but we'll find out by what this script says.  now in use on serene for time updates.

---
+++ scripts/system/update_system_time.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# a simple time update script that traps output from rdate, since we
+# get a lot of noise even when a successful update occurs, to whit:
+#  "rdate: Could not read data: Success"
+#
+# author: chris koeritz
+
+#hmmm: the generalized pattern of show output only on error is embodied below.  make it a function.
+
+outfile="$(mktemp /tmp/update_system_time.XXXXXX)"
+/usr/bin/rdate -s time.nist.gov &> "$outfile"
+retval=$?
+if [ $retval -ne 0 ]; then
+  echo "Actual error code $retval returned by rdate, with output:"
+  cat "$outfile" 
+fi
+rm -f "$outfile"
+
