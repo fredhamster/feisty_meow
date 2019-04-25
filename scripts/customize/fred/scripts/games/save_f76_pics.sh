8048673adc654e86f2b3fc1ef11621f336ea0562
Chris Koeritz <fred@gruntoes.com>
2019-04-25 08:00:31 -0400

    and some fixes for original blorp

--- scripts/customize/fred/scripts/games/save_f76_pics.sh
+++ scripts/customize/fred/scripts/games/save_f76_pics.sh
@@ -2,12 +2,25 @@
 
 source "$FEISTY_MEOW_SCRIPTS/core/launch_feisty_meow.sh"
 
-if [[ "$hostname" ~= clemems* ]]; then
+if [[ "$(hostname)" =~ clemems* ]]; then
   character=clemens_pc
 fi
-if [[ "$hostname" ~= orpheus* ]]; then
+if [[ "$(hostname)" =~ orpheus* ]]; then
   character=spoonburg_pc
 fi
 
-netcp ~/linx/wind_goods/My\ Games/Fallout\ 76/Photos/8f99c06443f04f6f8270604369bb78eb/*[0-9].png /z/walrus/media/pictures/metaverse/fallout_76/${character}/
+src_dir="~/linx/wind_goods/My\ Games/Fallout\ 76/Photos/8f99c06443f04f6f8270604369bb78eb"
+dest_dir="/z/walrus/media/pictures/metaverse/fallout_76/${character}"
+
+if [ ! -d "$src_dir" ]; then
+  echo "Could not find the source directory: $src_dir"
+  exit 1
+fi
+if [ ! -d "$dest_dir" ]; then
+  echo "Could not find the destination directory: $dest_dir"
+  exit 1
+fi
+
+netcp "$src_dir"/*[0-9].png "$dest_dir"/
+
 
