5ca960abd03fdbb99ab0a95f23d1512acc2e87bd
Chris Koeritz <fred@gruntose.com>
2019-04-25 14:32:01 -0400

    more fixies, more to come

--- scripts/customize/fred/scripts/games/save_f76_pics.sh
+++ scripts/customize/fred/scripts/games/save_f76_pics.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
+# copies the fallout 76 screenshots and images into our walruse metaverse hierarchy.
+
 source "$FEISTY_MEOW_SCRIPTS/core/launch_feisty_meow.sh"
 
 if [[ "$(hostname)" =~ clemems* ]]; then
-  character=clemens_pc
+  character=chronical_pc
 fi
 if [[ "$(hostname)" =~ orpheus* ]]; then
   character=spoonburg_pc
+fi
+if [ -z "$character" ]; then
+  echo Could not determine the proper character name to store the fallout 76
+  echo images under.  Can you add a hostname to this script?
+  echo "  => $0"
+  exit 1
 fi
 
 src_dir="~/linx/wind_goods/My\ Games/Fallout\ 76/Photos/8f99c06443f04f6f8270604369bb78eb"
