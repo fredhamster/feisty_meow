505dfd51ee93909bea508da334aa35d0b11d35b3
Chris Koeritz <fred@gruntoes.com>
2019-04-25 07:55:38 -0400

    added a splitter based on hostname
    avoids cross contamination of pictures from orpheus's characters (spoonburg and obsidian) and clemens's characters (antimony).

--- scripts/customize/fred/scripts/games/save_f76_pics.sh
+++ scripts/customize/fred/scripts/games/save_f76_pics.sh
@@ -1,5 +1,13 @@
+#!/bin/bash
 
 source "$FEISTY_MEOW_SCRIPTS/core/launch_feisty_meow.sh"
 
-netcp ~/linx/wind_goods/My\ Games/Fallout\ 76/Photos/8f99c06443f04f6f8270604369bb78eb/*[0-9].png /z/walrus/media/pictures/metaverse/fallout_76/spoonburg_pc/
+if [[ "$hostname" ~= clemems* ]]; then
+  character=clemens_pc
+fi
+if [[ "$hostname" ~= orpheus* ]]; then
+  character=spoonburg_pc
+fi
+
+netcp ~/linx/wind_goods/My\ Games/Fallout\ 76/Photos/8f99c06443f04f6f8270604369bb78eb/*[0-9].png /z/walrus/media/pictures/metaverse/fallout_76/${character}/
 
