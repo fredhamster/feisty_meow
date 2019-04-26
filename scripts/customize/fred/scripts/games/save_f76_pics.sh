b4e5a391343da3ebdf874acc145ac0f37382058d
Chris Koeritz <fred@gruntose.com>
2019-04-26 09:22:59 -0400

    fixes were needed still
    had a different unique id for chronical, plus a bunch of issues in directory path names (or one really, misspelling clemens)

--- scripts/customize/fred/scripts/games/save_f76_pics.sh
+++ scripts/customize/fred/scripts/games/save_f76_pics.sh
@@ -4,21 +4,24 @@
 
 source "$FEISTY_MEOW_SCRIPTS/core/launch_feisty_meow.sh"
 
-if [[ "$(hostname)" =~ clemems* ]]; then
-  character=chronical_pc
-fi
-if [[ "$(hostname)" =~ orpheus* ]]; then
-  character=spoonburg_pc
-fi
-if [ -z "$character" ]; then
+if [[ "$(hostname)" =~ clemens* ]]; then
+  character="chronical_pc"
+  identity="8836c852c8a647ba8ca45808a73c3fbb"
+elif [[ "$(hostname)" =~ orpheus* ]]; then
+  character="spoonburg_pc"
+  identity="8f99c06443f04f6f8270604369bb78eb"
+else
   echo Could not determine the proper character name to store the fallout 76
   echo images under.  Can you add a hostname to this script?
   echo "  => $0"
   exit 1
 fi
 
-src_dir="~/linx/wind_goods/My\ Games/Fallout\ 76/Photos/8f99c06443f04f6f8270604369bb78eb"
+src_dir="$HOME/linx/wind_goods/My Games/Fallout 76/Photos/${identity}"
 dest_dir="/z/walrus/media/pictures/metaverse/fallout_76/${character}"
+
+#echo "src is: '$src_dir'"
+#echo "dest is: '$dest_dir'"
 
 if [ ! -d "$src_dir" ]; then
   echo "Could not find the source directory: $src_dir"
