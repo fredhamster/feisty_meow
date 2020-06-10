e9dd58c1332357dd084f2773e93463f9d300ffcf
Chris Koeritz <fred@gruntose.com>
2020-06-10 15:33:50 -0400

    cleaner implementation of keep_awake
    will still put process in background, but now you can control it from the shell.
    unfortunately, these will probably still zombie out if the connection gets dropped for some reason (either because we're not printing frequently enough or for causes out of our control).

--- scripts/tty/keep_awake_process.sh
+++ scripts/tty/keep_awake_process.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
+
 # This program is meant to be started by the program keep_awake and has
-# the basic guts that are meant to execute inside of a semi-perpetual loop.
+# the guts that are meant to execute inside of a semi-perpetual loop.
 
 source "$FEISTY_MEOW_SCRIPTS/core/launch_feisty_meow.sh"
 
+#hmmm: is there still a process management thingy, referred to below, active in our scripts???
 # save the process id for the goodbye program to deal with.
 #echo $$ >>$TMP/trash.last_keep_awake_process
 #don't let the shutdown guy know who we are; we want to keep running now.
 
 # loop sort of forever.
 while true; do
-# this version is for keeping a modem awake.
-#  ping -c 7 www.gruntose.com >/dev/null
-
   echo "trying not to fall asleep at $(date_stringer)"
-  sleep 120
+  # magical number of seconds to sleep...
+  sleep 64
 done
