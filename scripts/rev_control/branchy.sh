d0e1480958985bb8d84cba626076f8f8a3c85344
Chris Koeritz <fred@gruntose.com>
2022-05-25 19:16:45 -0400

    branchy recurses!
    yeah!
    now a more expected behavior is exhibited if current directory has several repositories under it; each repo below or at current dir will be found (subject to depth limit).

--- scripts/rev_control/branchy.sh
+++ scripts/rev_control/branchy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# branchy: lists the branches active on all of the folders present in the
-# REPOSITORY_LIST variable or in paths provided on the command line.
+# branchy: lists the branches active on all of the paths provided on the command line.
+# if no directory is specified, this defaults to operating on the current directory.
 
 source "$FEISTY_MEOW_SCRIPTS/core/launch_feisty_meow.sh"
 source "$FEISTY_MEOW_SCRIPTS/rev_control/version_control.sh"
@@ -10,15 +10,29 @@ save_terminal_title
 
 ##############
 
-FULL_LIST="$@"
+## holding old implem:
+#FULL_LIST="$@"
+## use the current directory if no paths were provided.
+#if [ -z "$FULL_LIST" ]; then
+#  FULL_LIST="."
+#fi#
+#show_active_branch $FULL_LIST
+#exit_on_error "showing branches: $FULL_LIST"
 
-# use the current directory if no paths were provided.
-if [ -z "$FULL_LIST" ]; then
-  FULL_LIST="."
+dir="$1"; shift
+if [ -z "$dir" ]; then
+  dir=.
 fi
 
-show_active_branch $FULL_LIST
-exit_on_error "showing branches: $FULL_LIST"
+pushd "$dir" &>/dev/null
+exit_on_error "changing to directory: $dir"
+tempfile=$(generate_rev_ctrl_filelist)
+popd &>/dev/null
+
+perform_revctrl_action_on_file "$tempfile" show_active_branch
+exit_on_error "performing revision control action show_active_branch on: $tempfile"
+
+rm "$tempfile"
 
 ##############
 
