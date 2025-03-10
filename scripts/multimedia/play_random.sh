5428ee31232de0934191c38cfb625269e72af287
Fred Hamster <fred@gruntose.com>
2025-03-10 17:58:55 -0400

    some clean up on random play scripts
    still doesn't seem to be making sound though...

--- scripts/multimedia/play_random.sh
+++ scripts/multimedia/play_random.sh
@@ -1,30 +1,36 @@
 #!/usr/bin/env bash
-hostfile=/etc/HOSTNAME
-if [ ! -f "$hostfile" ]; then
-  hostfile=/etc/hostname
-fi
-if [ ! -f "$hostfile" ]; then
-  echo "Could not find the hostfile for this machine."
-  exit 3
-fi
 
-grunty=$(grep -i gruntose.blurgh <"$hostfile")
+# plays a random sound chosen from our sound library.
+
+#hmmm: very fred or feisty meow specific right now.
+
+our_host="$(hostname -f)"
+
+#hmmm: soooo antiquated and clunky!  just use a bash variable match expression to do this.
+grunty="$(echo "$our_host" | grep -i gruntose.blurgh)"
+
 if [ ! -z "$grunty" ]; then
   VOXDIR=/z/walrus/media/sounds
 #hmmm: generalize the above.
-  FILE_LIST=$(find $VOXDIR -type f)
-echo "file list is $FILE_LIST"
-  LINES=$(find $VOXDIR -type f | wc -l)
-#echo "info found is: $LINES"
-  LESS_CHANCE=$(expr $LINES \\* 4)
-#echo "less chance is $LESS_CHANCE"
-  TO_PLAY=
-  while [ -z "$TO_PLAY" ]; do
-    TO_PLAY=$(find $VOXDIR -type f | random $LESS_CHANCE | tail -1)
-#echo "to play is $TO_PLAY"
-  done
 
-  # now play the file
+#  FILE_LIST=$(find $VOXDIR -type f)
+#echo "file list is $FILE_LIST"
+#  LINES=$(find $VOXDIR -type f | wc -l)
+#echo "info found is: $LINES"
+#  LESS_CHANCE=$(expr $LINES \\* 4)
+#echo "less chance is $LESS_CHANCE"
+#  TO_PLAY=
+#  while [ -z "$TO_PLAY" ]; do
+#hmmm: random was used here, but not sure where that came from.
+#    TO_PLAY=$(find $VOXDIR -type f | random $LESS_CHANCE | tail -n 1)
+#hmmm: how was LESS_CHANCE being used previously?  => who cares?  this was bizarre and awful.
+
+  TO_PLAY="$(find $VOXDIR -type f | shuf | tail -n 1)"
+#echo "to play is $TO_PLAY"
+
+#  done
+
+  # now play the file we randomly chose.
   bash $FEISTY_MEOW_SCRIPTS/multimedia/sound_play.sh $TO_PLAY
 
 fi
--- scripts/multimedia/randomly_play.sh
+++ scripts/multimedia/randomly_play.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
-# this decides randomly whether to invoke the play_random script or not.
+
+# this decides randomly whether to invoke the play_random script or not.  it is a way to
+# make random sounds, but not on every single call.
+
+source "$FEISTY_MEOW_SCRIPTS/core/launch_feisty_meow.sh"
 
 NOISE_IT_UP=$(expr $RANDOM / 91)
   # range is 0 to 360 after the division.
 #echo noise lev is $NOISE_IT_UP
 if [ $NOISE_IT_UP -gt 108 -a $NOISE_IT_UP -le 128 ]; then
   # we hit our percentage.
-  bash $FEISTY_MEOW_SCRIPTS/play_random.sh
+  play_random
 fi
 
