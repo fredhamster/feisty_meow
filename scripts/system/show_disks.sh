0432f90a5f9d2493980d026be28374f438b83507
Fred T. Hamster <fred@feistymeow.org>
2026-03-05 13:27:52 -0500

    added nvme as a valid disk type to show

--- scripts/system/show_disks.sh
+++ scripts/system/show_disks.sh
@@ -4,6 +4,7 @@
 
 for device in \
     $(grep "sd.*[0-9]" /proc/partitions | awk '{ print "/dev/" $4 }') \
+    $(grep "nvme.*[0-9]" /proc/partitions | awk '{ print "/dev/" $4 }') \
 ; do
   echo
   echo
